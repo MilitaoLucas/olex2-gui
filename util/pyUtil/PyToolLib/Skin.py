7bf3f4655b351fc519399407c0070d91edbedb85
horst <horst@olexsys.org>
2009-03-24 18:06:11 +0000

    -- added Skin.py
    git-svn-id: http://svn.olex2.org/olex2-gui/trunk@2296 bceb41a0-0b25-4981-bef4-7ef80ce4c728

---
+++ util/pyUtil/PyToolLib/Skin.py
@@ -0,0 +1,41 @@
+from olexFunctions import OlexFunctions
+OV = OlexFunctions()
+import sys
+
+
+
+class Skin():
+  def __init__(self):
+    self.change()
+
+  def change(self):
+    skin = OV.FindValue('gui_skin_name')
+    skin_extension = OV.FindValue('gui_skin_extension')
+    try:
+      skin_path = "%s/util/pyUtil/PluginLib/skins/plugin-%sSkin" %(OV.BaseDir(), skin)
+      if skin_path not in sys.path:
+        sys.path.append("%s/util/pyUtil/PluginLib/skins/plugin-%sSkin" %(OV.BaseDir(), skin))
+      PilTools = __import__("PilTools_%s" %skin)
+      #print "pyTools -- Using %s skin." %"PilTools_%s: %s" %(skin, tool)
+    except ImportError:
+      #print "pyTools -- Using Default PilTools for Tool: %s" %tool 
+      import PilTools
+    except Exception, err:
+      raise
+    self.GuiSkinChanger_instance = PilTools.GuiSkinChanger()
+    self.timage_instance = PilTools.timage()
+    self.sNumTitle_instance = PilTools.sNumTitle()
+
+  def run_skin(self, f, args):
+    if f == 'timage':
+      self.timage_instance.run_timage()
+    elif f == 'sNumTitle':
+      self.sNumTitle_instance.run_sNumTitle()
+    elif f == 'change':
+      self.change()
+      self.GuiSkinChanger_instance.run_GuiSkinChanger()
+      self.timage_instance.run_timage()
+      self.sNumTitle_instance.run_sNumTitle()
+      
+Skin_instance = Skin()    
+OV.registerMacro(Skin_instance.run_skin, 'function-The function to call')
