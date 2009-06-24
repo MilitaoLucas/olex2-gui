90275a78f3bd53e3ddcba9da0cb1811786d994c6
horst <horst@olexsys.org>
2009-06-24 12:58:43 +0000

    -- crypto updated to Py2.6

--- util/pyUtil/PluginLib/plugin-Crypto/Crypto/Hash/MD5.py
+++ util/pyUtil/PluginLib/plugin-Crypto/Crypto/Hash/MD5.py
@@ -3,11 +3,18 @@
 
 __revision__ = "$Id: MD5.py,v 1.4 2002/07/11 14:31:19 akuchling Exp $"
 
-from md5 import *
 
-import md5
-if hasattr(md5, 'digestsize'):
-    digest_size = digestsize
-    del digestsize
-del md5
+import hashlib
+def new(data=""):
+    return hashlib.md5(data)
+digest_size = new().digest_size
+
+
+#from md5 import *
+
+#import md5
+#if hasattr(md5, 'digestsize'):
+    #digest_size = digestsize
+    #del digestsize
+#del md5
 
