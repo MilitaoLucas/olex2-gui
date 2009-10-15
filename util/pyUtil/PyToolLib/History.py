MilitaoLucas/olex2-gui

commit d7ee75470df89412974867ce716ee5a86c2193ae

A few changes/fixes to History; change some file readers so interface is consistent
git-svn-id: http://svn.olex2.org/olex2-gui/trunk@2705 bceb41a0-0b25-4981-bef4-7ef80ce4c728

util/pyUtil/PyToolLib/History.py
@@ -57,6 +57,8 @@ class History(ArgumentParser):
     if not tree:
       tree = HistoryTree()
       
+    if os.path.splitext(self.filefull)[-1].lower() == '.cif':
+      return # don't attempt to make a history if a cif is loaded
     filefull_lst = os.path.splitext(self.filefull)[0] + '.lst'
     if self.autochem or self.demo_mode:
       branchName = self.params.snum.history.autochem_next_solution
@@ -72,9 +74,8 @@ class History(ArgumentParser):
     self.setParams()
     return self.his_file
   
-  def delete_history(self, args):
+  def delete_history(self, del_solution):
     self._getItems()
-    del_solution = args.get('solution')
     if not del_solution.strip():
       return
     if self.params.user.alert_delete_history[0] == 'R':
@@ -227,7 +228,10 @@ class History(ArgumentParser):
     self.params.snum.history.next_solution = 'Solution 01'
     self._createNewHistory()
     self.setParams()
-    
+
+  def is_empty(self):
+    return tree.historyTree == {}
+
   def _createNewHistory(self):
     self.filename = olx.FileName()
     historyPicklePath = '/'.join([self.strdir,'%s.history' %self.filename])
@@ -331,17 +335,22 @@ class History(ArgumentParser):
     historyTextList.append("<br>")
     number_of_solutions = len(tree.historyTree.keys())
     if number_of_solutions > 1 and 'Autochem' not in self.params.snum.history.current_solution:
-      historyTextList.append("<a href='spy.delete_history -solution=\"GetValue(SET_HISTORY_CURRENT_SOLUTION)\">>UpdateHtml' target='Delete current history'><zimg border='0' src='delete_small.png'></a>")
+      historyTextList.append("<a href='spy.delete_history(\"GetValue(SET_HISTORY_CURRENT_SOLUTION)\")>>UpdateHtml' target='Delete current history'><zimg border='0' src='delete_small.png'></a>")
     if not self.demo_mode:
-      historyTextList.append(' <b>%s</b> -  %s - %s' %(curr_history_branch.spaceGroup,curr_history_branch.solution_program,curr_history_branch.solution_method))
+      solution = curr_history_branch.historyBranch.get('solution')
+      if solution is not None:
+        historyTextList.append(' <b>%s</b> -  %s - %s' %(
+          curr_history_branch.spaceGroup, solution.solution_program, solution.solution_method))
+      else:
+        historyTextList.append(' <b>%s</b>' %curr_history_branch.spaceGroup)
     
     historyText = '\n'.join(historyTextList)
 #    if self.demo_mode:
 #      historyText = ""
     OV.write_to_olex('history-info.htm',historyText)
-  
+
 hist = History()
-OV.registerMacro(hist.delete_history, 'solution')
+OV.registerFunction(hist.delete_history)
 OV.registerMacro(hist.rename_history, 'old-Old refinement name&;new-New refinement name')
 OV.registerMacro(hist.revert_history, 'solution-&;refinement-')
 OV.registerFunction(hist.create_history)
@@ -408,25 +417,27 @@ class HistoryTree:
     hist._make_history_bars()
     hist.setParams()
     return self.current_solution
-  
+
   def newLeaf(self,resPath,lstPath):
     self.current_refinement = self.historyTree[self.current_solution].newLeaf(resPath,lstPath)
     return self.current_refinement
-  
-  
+
+
 class HistoryBranch:
   def __init__(self,resPath,lstPath,solution=True):
-    self.spaceGroup = OV.olex_function('sg(%n)')
+    self.spaceGroup = OV.GetParam('snum.refinement.sg')
     self.historyBranch = {}
     if solution:
       tree.current_refinement = 'solution'
       self.historyBranch['solution'] = HistoryLeaf(resPath,lstPath)
+      self.solution_program = self.historyBranch['solution'].solution_program
+      self.solution_method = self.historyBranch['solution'].solution_method
     else:
       self.newLeaf(resPath,lstPath)
-    self.solution_program = OV.GetParam('snum.solution.program')
-    self.solution_method = OV.GetParam('snum.solution.method')
+      self.solution_program = None
+      self.solution_method = None
     self.name = None
-    
+
   def newLeaf(self,resPath,lstPath):
     ref_num = str(len(self.historyBranch.keys()) + 1)
     if len(ref_num) == 1:
@@ -436,13 +447,14 @@ class HistoryBranch:
     self.historyBranch[ref_name] = HistoryLeaf(resPath,lstPath)
     return ref_name
 
+
 class HistoryLeaf:
   def __init__(self,resPath,lstPath):
-    self.solution_program = 'n/a'
-    self.solution_method = 'n/a'
-    self.refinement_program = 'n/a'
-    self.refinement_method = 'n/a'
-    self.program_version = 'n/a'
+    self.solution_program = None
+    self.solution_method = None
+    self.refinement_program = None
+    self.refinement_method = None
+    self.program_version = None
     self.R1 = 'n/a'
     self.wR2 = 'n/a'
     self.lst = None
@@ -451,10 +463,10 @@ class HistoryLeaf:
     self.res = compressFile(resPath)
     ref_program = OV.GetParam('snum.refinement.program')
     sol_program = OV.GetParam('snum.solution.program')
-    if tree.current_refinement == 'solution' and 'smtbx' in sol_program:
+    if tree.current_refinement == 'solution' and sol_program == 'smtbx-solve':
       self.solution_program = sol_program
       self.solution_method = OV.GetParam('snum.solution.method')
-    elif tree.current_refinement != 'solution' and 'smtbx' in ref_program:
+    elif tree.current_refinement != 'solution' and sol_program == 'smtbx-solve':
       self.refinement_program = ref_program
       self.refinement_method = OV.GetParam('snum.refinement.method')
       try:
@@ -464,47 +476,56 @@ class HistoryLeaf:
         
     elif os.path.exists(lstPath):
       self.lst = compressFile(lstPath)
-      self.getLeafInfo(lstPath)
+      self.getLeafInfoFromLst(lstPath)
     else:
-      self.getLeafInfo(resPath)
-      
-  def getLeafInfo(self,filePath):
-    if os.path.splitext(filePath)[-1] == '.lst':
-      try:
-        lst_file = open(filePath)
-        lstValues = lst_reader.reader(lst_file).values()
-        lst_file.close()
-      except:
-        lstValues = {'R1':'','wR2':''}
-      try:
-        self.R1 = float(lstValues.get('R1', 'n/a'))
-        self.wR2 = float(lstValues.get('wR2', 'n/a'))
-      except ValueError:
-        self.R1 = 'n/a'
-        self.wR2 = 'n/a'
-        
-      if self.R1 == 'n/a':
-        self.solution_program = OV.GetParam('snum.solution.program')
-        self.solution_method = OV.GetParam('snum.solution.method')
-      else:
-        self.refinement_program = OV.GetParam('snum.refinement.program')
-        self.refinement_method = OV.GetParam('snum.refinement.method')
-        
-      self.program_version = lstValues.get('version',None)
-      
-    elif os.path.splitext(filePath)[-1] in ('.res', '.ins'):
-      try:
-        iresValues = ires_reader.reader(open(filePath)).values()
-      except:
-        iresValues = {'R1':''}
-      try:
-        self.R1 = float(iresValues['R1'])
-      except ValueError:
-        self.R1 = 'n/a'
-      self.wR2 = 'n/a'
-      self.refinement_method = 'n/a'
-      self.refinement_program = 'n/a'
+      self.getLeafInfoFromRes(resPath)
     OV.SetParam('snum.refinement.last_R1', self.R1)
+    if tree.current_refinement == 'solution':
+      if self.solution_program is None:
+        self.solution_program = OV.GetParam('snum.solution.program')
+      if self.solution_method is None:
+        self.solution_method = OV.GetParam('snum.solution.method')
+    else:
+      if self.refinement_program is None:
+        self.refinement_program = OV.GetParam('snum.refinement.program')
+      if self.refinement_method is None:
+        self.refinement_method = OV.GetParam('snum.refinement.method')
+
+  def getLeafInfoFromLst(self, filePath):
+    try:
+      lst_file = open(filePath)
+      lstValues = lst_reader.reader(lst_file).values()
+      lst_file.close()
+    except:
+      lstValues = {'R1':'','wR2':''}
+    try:
+      self.R1 = float(lstValues.get('R1', 'n/a'))
+      self.wR2 = float(lstValues.get('wR2', 'n/a'))
+    except ValueError:
+      self.R1 = 'n/a'
+      self.wR2 = 'n/a'
+      
+    if self.R1 == 'n/a':
+      self.solution_program = OV.GetParam('snum.solution.program')
+      self.solution_method = OV.GetParam('snum.solution.method')
+    else:
+      self.refinement_program = OV.GetParam('snum.refinement.program')
+      self.refinement_method = OV.GetParam('snum.refinement.method')
+      
+    self.program_version = lstValues.get('version',None)
+
+  def getLeafInfoFromRes(self, filePath):
+    try:
+      iresValues = ires_reader.reader(open(filePath)).values()
+    except:
+      iresValues = {'R1':''}
+    try:
+      self.R1 = float(iresValues['R1'])
+    except ValueError:
+      self.R1 = 'n/a'
+    self.wR2 = 'n/a'
+    self.refinement_method = None
+    self.refinement_program = None
 
   def setLeafInfo(self):
     OV.SetParam('snum.refinement.last_R1',self.R1)

etc/gui/solution-settings-only.htm
@@ -162,7 +162,7 @@
 				name='SET_SNUM_REFINEMENT_SPACE_GROUP',
 				items='$spy.GetParam(snum.refinement.sg_list)',
 				value='$sg(%n)',
-				onchange="reset -s=GetValue(SET_SNUM_REFINEMENT_SPACE_GROUP) -c=GetValue(SET_SNUM_REFINEMENT_FORMULA)>>spy.Skin sNumTitle>>updatehtml"
+				onchange="reset -s=GetValue(SET_SNUM_REFINEMENT_SPACE_GROUP) -c=GetValue(SET_SNUM_REFINEMENT_FORMULA)>>spy.SetParam(snum.refinement.sg,sg(%n))>>spy.Skin sNumTitle>>updatehtml"
 			>
 		</font>
 	</td>

util/pyUtil/PyToolLib/CifInfo.py
@@ -117,7 +117,6 @@ class MetaCif(ArgumentParser):
     """Returns a string in cif format of all items in a dictionary of cif items."""
 
     listText = []
-
     name_value_pairs = olx.phil_handler.name_value_pairs('snum.metacif')
     for name, value in name_value_pairs:
       if 'file' in name or 'user_input' in name:
@@ -131,7 +130,7 @@ class MetaCif(ArgumentParser):
           continue
         elif value == '?':
           continue
-        elif cifName == '_publ_author_names' and OV.GetParam('snum.metacif.publ_author_names') != '?':
+        elif cifName == '_publ_author_names' and OV.GetParam_as_string('snum.metacif.publ_author_names') != '?':
           loop = [('_publ_author_name','_publ_author_email','_publ_author_address')]
           names = value.split(';')
           for name in names:
@@ -232,7 +231,7 @@ class MetaCif(ArgumentParser):
             i+=1
           value += "\n;"
           try:
-            oldValue = OV.GetParam('snum.metacif.%s' %field)
+            oldValue = OV.GetParam_as_string('snum.metacif.%s' %field)
             if oldValue is not None and value != str(oldValue).rstrip().lstrip():
               OV.SetParam('snum.metacif.%s' %field,value)
               variableFunctions.AddVariableToUserInputList('%s' %field)
@@ -252,7 +251,7 @@ class MetaCif(ArgumentParser):
             value = x[0]
           value = value.replace("'", "").rstrip()
           try:
-            oldValue = OV.GetParam('snum.metacif.%s' %field)
+            oldValue = OV.GetParam_as_string('snum.metacif.%s' %field)
             if oldValue is not None and value != str(oldValue).strip():
               OV.SetParam('snum.metacif.%s' %field, value)
               variableFunctions.AddVariableToUserInputList('%s' %field)
@@ -351,10 +350,11 @@ class CifTools(ArgumentParser):
     try:
       solution_branch = History.tree.historyTree[History.tree.current_solution]
       current_refinement = solution_branch.historyBranch[History.tree.current_refinement]
-      solution_reference = SPD.programs[solution_branch.solution_program].reference
-      OV.SetParam('snum.metacif.computing_structure_solution', solution_reference)
-      atom_sites_solution_primary = SPD.programs[solution_branch.solution_program].methods[solution_branch.solution_method].atom_sites_solution
-      OV.SetParam('snum.metacif.atom_sites_solution_primary', atom_sites_solution_primary)
+      if solution_branch.historyBranch.has_key('solution'):
+        solution_reference = SPD.programs[solution_branch.solution_program].reference
+        OV.SetParam('snum.metacif.computing_structure_solution', solution_reference)
+        atom_sites_solution_primary = SPD.programs[solution_branch.solution_program].methods[solution_branch.solution_method].atom_sites_solution
+        OV.SetParam('snum.metacif.atom_sites_solution_primary', atom_sites_solution_primary)
     except KeyError:
       pass
 
@@ -363,8 +363,8 @@ class CifTools(ArgumentParser):
     if "frames" in tools:
       p = self.sort_out_path(path, "frames")
       if p != "File Not Found" and self.metacifFiles.curr_frames != self.metacifFiles.prev_frames:
-        from bruker_frames import BrukerFrame
-        frames = BrukerFrame(p).cifItems()
+        import bruker_frames
+        frames = bruker_frames.reader(p).cifItems()
         merge.append(frames)
 
     if "smart" in tools:
@@ -399,8 +399,8 @@ class CifTools(ArgumentParser):
     if "saint" in tools:
       p = self.sort_out_path(path, "saint")
       if p != "File Not Found" and self.metacifFiles.curr_saint != self.metacifFiles.prev_saint:
-        from bruker_saint import bruker_saint
-        saint = bruker_saint(p).read_saint()
+        import bruker_saint
+        saint = bruker_saint.reader(p).cifItems()
         computing_cell_refinement = self.prepare_computing(saint, versions, "saint")
         saint.setdefault("_computing_cell_refinement", computing_cell_refinement)
         computing_data_reduction = self.prepare_computing(saint, versions, "saint")
@@ -411,8 +411,8 @@ class CifTools(ArgumentParser):
       p = self.sort_out_path(path, "sad")
       if p != "File Not Found" and self.metacifFiles.curr_sad != self.metacifFiles.prev_sad:
         try:
-          from sadabs import Sadabs
-          sad = Sadabs(p).read_sad()
+          import sadabs
+          sad = sadabs.reader(p).cifItems()
           version = self.prepare_computing(sad, versions, "sad")
           version = string.strip((string.split(version, "="))[0])
           t = self.prepare_exptl_absorpt_process_details(sad, version)

util/pyUtil/PyToolLib/ExternalPrgParameters.py
@@ -312,18 +312,19 @@ class Method_shelx_refinement(Method_shelx, Method_refinement):
     Method_shelx.__init__(self, name, cmd, args, atom_sites_solution)
   
   def pre_refinement(self, RunPrgObject):
-    diffrn_ambient_temperature = OV.FindValue('snum.metacif.diffrn_ambient_temperature')
-    if '(' in diffrn_ambient_temperature:
-      diffrn_ambient_temperature = diffrn_ambient_temperature.split('(')[0]
-    if 'K' in diffrn_ambient_temperature:
-      diffrn_ambient_temperature = diffrn_ambient_temperature.split('K')[0]
-    try:
-      diffrn_ambient_temperature = float(diffrn_ambient_temperature)
-      diffrn_ambient_temperature = diffrn_ambient_temperature - 273.0
-      OV.DelIns('TEMP')
-      OV.AddIns('TEMP %s' %diffrn_ambient_temperature)
-    except:
-      pass
+    diffrn_ambient_temperature = OV.GetParam('snum.metacif.diffrn_ambient_temperature')
+    if diffrn_ambient_temperature is not None:
+      if '(' in diffrn_ambient_temperature:
+        diffrn_ambient_temperature = diffrn_ambient_temperature.split('(')[0]
+      if 'K' in diffrn_ambient_temperature:
+        diffrn_ambient_temperature = diffrn_ambient_temperature.split('K')[0]
+      try:
+        diffrn_ambient_temperature = float(diffrn_ambient_temperature)
+        diffrn_ambient_temperature = diffrn_ambient_temperature - 273.0
+        OV.DelIns('TEMP')
+        OV.AddIns('TEMP %s' %diffrn_ambient_temperature)
+      except:
+        pass
     Method_refinement.pre_refinement(self, RunPrgObject)
 
   def observe(self, RunPrgObject):

util/pyUtil/PyToolLib/FileReaders/bruker_frames.py
@@ -1,103 +1,5 @@
 # bruker_frames.py
 
-class BrukerFrame(object):
-	def __init__(self, filepath):
-		self._cifItems = {}
-		self._saint_cfg = {}
-		rFile = open(filepath, 'r')
-		self.data = rFile.read()
-		rFile.close()
-		self.readHeader()
-		
-	def readHeader(self):
-		value = ""
-		previousHeader = ""
-		header_d = {}
-		m = 0
-		while "............................" not in self.data[m:m+80]:
-			try:
-				n = m + 8
-				header = self.data[m:n]
-				m = n
-				n = m + 72
-				value = self.data[m:n]
-				m = n
-				
-				header = header.strip(':').strip()
-				value = value.strip()
-				if header == "":
-					break
-				if header != previousHeader:
-					header_d.setdefault(header,value)
-				else:
-					header_d[header] += " %s" %value
-				previousHeader = header
-				
-			except:
-				break
-			
-		a = header_d['CSIZE'].split('|')
-		a.sort()
-		size = []
-		for i in a:
-			try:
-				float(i)
-				size.append(i)
-			except:
-				pass
-			
-		matrix = header_d['MATRIX'].split()
-		
-		self._cifItems['_diffrn_source_voltage'] = "%.0f" %float(header_d['SOURCEK'])
-		self._cifItems['_diffrn_source_current'] = "%.0f" %float(header_d['SOURCEM'])
-		self._cifItems['_diffrn_refln_scan_width'] = "%.2f" %float(header_d['RANGE'])
-		self._cifItems['time'] = "%.2f" %float(header_d['CUMULAT'])
-		self._cifItems['formula'] = "%s" %(header_d['CHEM'])
-		self._cifItems['_diffrn_radiation_monochromator'] = header_d['FILTER']
-		self._cifItems['_diffrn_radiation_wavelength'] = "%f" %float(header_d['WAVELEN'].split()[0])
-		self._cifItems['_exptl_crystal_description'] = "%s" %(header_d['MORPH'])
-		self._cifItems['_exptl_crystal_colour'] = "%s" %(header_d['CCOLOR'])
-		if len(size) == 3:
-			self._cifItems['_exptl_crystal_size_min'] = "%s" %(size[0])
-			self._cifItems['_exptl_crystal_size_mid'] = "%s" %(size[1])
-			self._cifItems['_exptl_crystal_size_max'] = "%s" %(size[2])
-		self._cifItems['_diffrn_orient_matrix_UB_11'] = "%f" %float(matrix[0])
-		self._cifItems['_diffrn_orient_matrix_UB_12'] = "%f" %float(matrix[1])
-		self._cifItems['_diffrn_orient_matrix_UB_13'] = "%f" %float(matrix[2])
-		self._cifItems['_diffrn_orient_matrix_UB_21'] = "%f" %float(matrix[3])
-		self._cifItems['_diffrn_orient_matrix_UB_22'] = "%f" %float(matrix[4])
-		self._cifItems['_diffrn_orient_matrix_UB_23'] = "%f" %float(matrix[5])
-		self._cifItems['_diffrn_orient_matrix_UB_31'] = "%f" %float(matrix[6])
-		self._cifItems['_diffrn_orient_matrix_UB_32'] = "%f" %float(matrix[7])
-		self._cifItems['_diffrn_orient_matrix_UB_33'] = "%f" %float(matrix[8])
-		
-		ccdparam = header_d['CCDPARM'].split()
-		self._saint_cfg.setdefault("READNOISE", ccdparam[0])
-		self._saint_cfg.setdefault("EPERADU", ccdparam[1])
-		self._saint_cfg.setdefault("EPERPHOTON", ccdparam[2])
-		
-		ccdparam = header_d['DETTYPE'].split()
-		if ccdparam[0] == "CCD-PXL-L6000":
-			self._saint_cfg.setdefault("PIXPERCM", ccdparam[1])
-			self._saint_cfg.setdefault("CM_TO_GRID", ccdparam[2])
-			self._saint_cfg.setdefault("BRASS_SPACING", ccdparam[4])
-			self._saint_cfg.setdefault("D_ATTENUATION", "31.1977")
-			self._cifItems.setdefault("_diffrn_measurement_device_type", "BRUKER SMART CCD 6000")
-			self._cifItems.setdefault("_diffrn_detector_area_resol_mean", "8")
-			
-		if ccdparam[0] == "CCD-PXL":
-			self._saint_cfg.setdefault("PIXPERCM", 81.920)
-			self._saint_cfg.setdefault("CM_TO_GRID", 0.800)
-			self._saint_cfg.setdefault("BRASS_SPACING", 0.2540)
-			self._cifItems.setdefault("_diffrn_measurement_device_type", "BRUKER SMART CCD 1000")
-			self._cifItems.setdefault("_diffrn_detector_area_resol_mean", "8")
-			
-	def cifItems(self):
-		return self._cifItems
-	
-	def saint_cfg(self):
-		return self._saint_cfg
-
 class reader:
 	def __init__(self, path):
 		self._cifItems = {}
@@ -192,110 +94,8 @@ class reader:
 	
 	def saint_cfg(self):
 		return self._saint_cfg
-		
 
-#class bruker_frame:
-	#def __init__(self, path):
-		#self.path = path
-		
-	#def read_frame(self):
-		#rFile = open(self.path, 'r')
-		#value = ""
-		#previousHeader = ""
-		#header_d = {}
-		#data = rFile.read()
-		#m = 0
-		#while "............................" not in data[m:m+80]:
-			#try:
-				#n = m + 8
-				#header = data[m:n]
-				#m = n
-				#n = m + 72
-				#value = data[m:n]
-				#m = n
-				
-				#header = header.strip(':').strip()
-				#value = value.strip()
-				#if header == "":
-					#break
-				#if header != previousHeader:
-					#header_d.setdefault(header,value)
-				#else:
-					#header_d[header] += " %s" %value
-				#previousHeader = header
-				
-			#except:
-				#break
-			
-		#a = header_d['CSIZE'].split('|')
-		#a.sort()
-		#size = []
-		#for i in a:
-			#try:
-				#float(i)
-				#size.append(i)
-			#except:
-				#pass
-			
-		#matrix = header_d['MATRIX'].split()
-		
-		#frame = {}
-		#frame['_diffrn_source_power'] = "%.0f" %float(header_d['SOURCEK'])
-		#frame['_diffrn_source_current'] = "%.0f" %float(header_d['SOURCEM'])
-		#frame['_diffrn_refln_scan_width'] = "%.2f" %float(header_d['RANGE'])
-		#frame['time'] = "%.2f" %float(header_d['CUMULAT'])
-		#frame['formula'] = "%s" %(header_d['CHEM'])
-		#frame['_diffrn_radiation_monochromator'] = header_d['FILTER']
-		#frame['_diffrn_radiation_wavelength'] = "%f" %float(header_d['WAVELEN'].split()[0])
-		#frame['_exptl_crystal_description'] = "%s" %(header_d['MORPH'])
-		#frame['_exptl_crystal_colour'] = "%s" %(header_d['CCOLOR'])
-		#if len(size) == 3:
-			#frame['_exptl_crystal_size_min'] = "%s" %(size[0])
-			#frame['_exptl_crystal_size_mid'] = "%s" %(size[1])
-			#frame['_exptl_crystal_size_max'] = "%s" %(size[2])
-		#frame['_diffrn_orient_matrix_UB_11'] = "%f" %float(matrix[0])
-		#frame['_diffrn_orient_matrix_UB_12'] = "%f" %float(matrix[1])
-		#frame['_diffrn_orient_matrix_UB_13'] = "%f" %float(matrix[2])
-		#frame['_diffrn_orient_matrix_UB_21'] = "%f" %float(matrix[3])
-		#frame['_diffrn_orient_matrix_UB_22'] = "%f" %float(matrix[4])
-		#frame['_diffrn_orient_matrix_UB_23'] = "%f" %float(matrix[5])
-		#frame['_diffrn_orient_matrix_UB_31'] = "%f" %float(matrix[6])
-		#frame['_diffrn_orient_matrix_UB_32'] = "%f" %float(matrix[7])
-		#frame['_diffrn_orient_matrix_UB_33'] = "%f" %float(matrix[8])
-		
-		#saint_cfg = {}
-		
-		#ccdparam = header_d['CCDPARM'].split()
-		#saint_cfg.setdefault("READNOISE", ccdparam[0])
-		#saint_cfg.setdefault("EPERADU", ccdparam[1])
-		#saint_cfg.setdefault("EPERPHOTON", ccdparam[2])
-		
-		#ccdparam = header_d['DETTYPE'].split()
-		#if ccdparam[0] == "CCD-PXL-L6000":
-			#saint_cfg.setdefault("PIXPERCM", ccdparam[1])
-			#saint_cfg.setdefault("CM_TO_GRID", ccdparam[2])
-			#saint_cfg.setdefault("BRASS_SPACING", ccdparam[4])
-			#saint_cfg.setdefault("D_ATTENUATION", "31.1977")
-			#frame.setdefault("_diffrn_measurement_device_type", "BRUKER SMART-CCD 6K")
-			#frame.setdefault("_diffrn_detector_area_resol_mean", "8")
-			
-		#if ccdparam[0] == "CCD-PXL":
-			#saint_cfg.setdefault("PIXPERCM", 81.920)
-			#saint_cfg.setdefault("CM_TO_GRID", 0.800)
-			#saint_cfg.setdefault("BRASS_SPACING", 0.2540)
-			#frame.setdefault("_diffrn_measurement_device_type", "BRUKER SMART-CCD 1K")
-			#frame.setdefault("_diffrn_detector_area_resol_mean", "8")
-		#self.saint_cfg = saint_cfg
-		
-		#return frame
-	
 if __name__ == '__main__':
-	#a = bruker_frame('C:/datasets/08srv071/frm071_1.001')
-	#frame = a.read_frame()
-	#a = reader('C:/datasets/08srv071/frm071_1.001')
-	#print a.cifItems()
-	#print a.saint_cfg()
 	a = BrukerFrame('C:/datasets/08srv071/frm071_1.001')
 	print a.cifItems()
 	print a.saint_cfg()
-	

util/pyUtil/PyToolLib/FileReaders/bruker_saint.py
@@ -1,16 +1,13 @@
 # bruker_saint.py
 
-class bruker_saint:
-	def __init__(self, path):
-		self.path = path
-		
-	def read_saint(self):		
+class reader:
+	def __init__(self, path):	
 		"""Reads the saint.ini file with the given path.
 		
 		Returns a dictionary of cif items found in the saint.ini file."""
 		
-		saint = {}
-		rfile = open(self.path, 'r')
+		self._cifItems = {}
+		rfile = open(path, 'r')
 		lines = {}
 		
 		i = 0
@@ -22,17 +19,17 @@ class bruker_saint:
 		for line in lines:
 			try:
 				if lines[i][:7] == "VERSION":
-					saint.setdefault("prog_version", lines[i][-6:])
+					self._cifItems.setdefault("prog_version", lines[i][-6:])
 					
 			except:
 				i += 1
 				pass
 			i += 1
-		self.saint_d = saint    
-		return saint   
-	
+
+	def cifItems(self):
+		return self._cifItems
+
 if __name__ == '__main__':
 	a = bruker_saint('C:/datasets/08srv071/work/saint.ini')
-	saint = a.read_saint()
+	saint = a.cifItems()
 	print
-	

util/pyUtil/olexex.py
@@ -156,7 +156,6 @@ class SpyVar(object):
 class OlexAtoms(object):
   def __init__(self):
     self.id_for_name = {}
-    #self.atoms = []
     self.atoms = [atom for atom in self.iterator()]
     
   def iterator(self):
@@ -177,26 +176,19 @@ class OlexAtoms(object):
       
       
       if name[:1] != "Q" and olx.xf_au_IsAtomDeleted(i) == "false":
-        #yield name, xyz, u, type, occu
         yield atom
-        #self.id_for_name[name] = str(i)
-        #self.atoms.setdefault(name, atom)
         
   def olexAtoms(self):
-    #return [i for i in self.iterator()]
     return self.atoms
   
   def numberAtoms(self):
-    #return sum(atom['occu'] for atom in self.iterator())
     return sum(atom['occu'] for atom in self.atoms)
   
   def number_non_hydrogen_atoms(self):
-    #return sum(atom['occu'] for atom in self.iterator() if atom['type'] not in ('H','Q'))
     return sum(atom['occu'] for atom in self.atoms if atom['type'] not in ('H','Q'))
   
   def currentFormula(self):
     curr_form = {}
-    #for atom in self.iterator():
     for atom in self.atoms:
       atom_type = atom['type']
       atom_occu = atom['occu']
@@ -205,7 +197,6 @@ class OlexAtoms(object):
     return curr_form
   
   def getExpectedPeaks(self):
-    #olex_atoms = [i for i in OlexAtoms().iterator()]
     cell_volume = float(olx.xf_au_GetVolume())
     expected_atoms = cell_volume/15
     present_atoms = self.numberAtoms()

commit 7403720ce0a19bc3b36ea3b8b902747a70c9a40d

fix bug introduced by r2705

util/pyUtil/PyToolLib/History.py
@@ -530,8 +530,8 @@ class HistoryLeaf:
   def setLeafInfo(self):
     OV.SetParam('snum.refinement.last_R1',self.R1)
     OV.SetParam('snum.last_wR2',self.wR2)
-    if self.refinement_program != 'n/a':
-      if self.refinement_method != 'n/a':
+    if self.refinement_program is not None:
+      if self.refinement_method is not None:
         OV.SetRefinementProgram(self.refinement_program, self.refinement_method)
       else:
         OV.SetRefinementProgram(self.refinement_program)