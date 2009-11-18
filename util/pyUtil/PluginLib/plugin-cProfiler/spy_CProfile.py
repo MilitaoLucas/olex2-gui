678421da6f9cdf8f88e378e671202be1f9018286
Richard <Richard>
2009-11-18 12:07:32 +0000

    disable include of cctbx.htm from tools, since doesn't work; minor modifications to old-style spy and cprofile version
    git-svn-id: http://svn.olex2.org/olex2-gui/trunk@2812 bceb41a0-0b25-4981-bef4-7ef80ce4c728

--- util/pyUtil/PluginLib/plugin-cProfiler/spy_CProfile.py
+++ util/pyUtil/PluginLib/plugin-cProfiler/spy_CProfile.py
@@ -1,6 +1,4 @@
 # spy_CProfile.py
-
-from spy import Spy
 import sys
 from olexFunctions import OlexFunctions
 OV = OlexFunctions()
@@ -16,18 +14,17 @@ class Spy(object):
   def run(self):
     g = OV.cprofile_wrap(self.spy_run)
     g()
-    
-    
+
   def spy_run(self):
     try:
       from pyTools import pyTools
       t = pyTools(self.tool, self.fun, self.param)
       t.run()
     except Exception, ex:
-      basedir = olx.BaseDir()
+      basedir = OV.BaseDir()
       rFile = open(r"%s/version.txt" %basedir)
       version = rFile.readline()
-      print >> sys.stderr, "===================================== Gui SVN Version: %s -- Olex Compilation Date: %s" %(version, olx.GetCompilationInfo())
+      print >> sys.stderr, "===================================== Gui SVN Version: %s -- Olex Compilation Date: %s" %(version, OV.GetCompilationInfo())
       print >> sys.stderr, "A Python Error has occured."
       print >> sys.stderr, "Tool: %s, Function: %s, Parameters: %s"
       sys.stderr.formatExceptionInfo()
