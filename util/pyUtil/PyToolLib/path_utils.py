b537b232bae1cb1cc75fcdad5b37e40aff1199f3
Richard <Richard>
2009-12-01 15:00:54 +0000

    cctbx_compatible_version changed to 2009_11_30_2204
    git-svn-id: http://svn.olex2.org/olex2-gui/trunk@2842 bceb41a0-0b25-4981-bef4-7ef80ce4c728

--- util/pyUtil/PyToolLib/path_utils.py
+++ util/pyUtil/PyToolLib/path_utils.py
@@ -41,7 +41,7 @@ def setup_cctbx():
     cctbx_TAG_file = open("%s/TAG" %cctbxSources,'r')
     cctbx_compile_date = cctbx_TAG_file.readline().strip()
     cctbx_TAG_file.close()
-    cctbx_compatible_version = "2009_09_16_0000"
+    cctbx_compatible_version = "2009_11_30_2204"
     if int(cctbx_compile_date.replace('_','')) < int(cctbx_compatible_version.replace('_','')):
       sys.stdout.write("""Warning: An incompatible version of the cctbx is installed.
 Please update to cctbx build '%s' or later.
