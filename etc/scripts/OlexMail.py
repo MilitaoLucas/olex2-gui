bdfd75bcd17d86fbcfbdc893c2805a003993f53b
xrayman <xrayman>
2009-12-04 11:42:33 +0000

    * Very basic emailer * My old one is somewhat out of date so replace chunks of code * Concept: 	User types spy.OlexMail() 	gets help 	User types spy.OlexMail(1) 	Sends the PythonError.log file to a preprogrammed address * Needs: 	Built-in: 		PythonError.log path 		usettings.dat path * Adds email capability * Adds two new variables to usettings.dat, not certain how Olex2 will cope? 	They are: 		email_address; and 		mailserver * Tested under Fedora 11 works fine using sendmail localhost server * Have used similar code in the past at Daresbury and worked find with internal exchange server as long as sender email address valid address on server - that is no relaying * No easy way to do this via smtp and googlemail due to requirement for passwords unless build in some encryption * + we want to know the sender for feedback

---
+++ etc/scripts/OlexMail.py
@@ -0,0 +1,77 @@
+import sys
+import os
+import smtplib
+import mimetypes
+from email.Encoders import encode_base64
+from email.MIMEAudio import MIMEAudio
+from email.MIMEBase import MIMEBase
+from email.MIMEImage import MIMEImage
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+import olex
+import olx
+from olexFunctions import OlexFunctions
+OV = OlexFunctions()
+
+
+def OlexMail(SendorNot=0):
+  if SendorNot <= 0:
+    print "This will attempt to email your PythonError.log to the Olex2 boys"
+    print "If this is what you want please check you have setup your:"
+    print "email address as email_address=; and"
+    print "mailserver information as mailserver="
+    print "In the usettings.dat file"
+    return
+  # I need to replace these with inbuilt olex2 functions such as usettings.dat path
+  # and ErrorLog path
+  recipients_address = "olex2pythonerrorlog@googlemail.com"
+  Olex2Path = "/home/xray/olex/" # these need to be inbuilt
+  ErrorLogPath = "/home/xray/.olexdata/0cde599287bbeeba7921594da41abef8/" # these need to be inbuilt
+  usettings = open("%s/usettings.dat"%(Olex2Path), 'r')
+  email_address = ""
+  mailserver = ""
+
+  for usettings_line in usettings:
+    if not email_address or not mailserver:
+      if "email_address" in usettings_line:
+        email_address = usettings_line.split("=")[-1].strip()
+        print "email_address = ", email_address
+      elif "mailserver" in usettings_line:
+        mailserver = usettings_line.split("=")[-1].strip()
+        print "mailserver = ", mailserver
+    else:
+      print "Mail and server", email_address, mailserver
+      break
+  usettings.close()
+
+  From = email_address
+  To = recipients_address
+  msg = MIMEMultipart()
+  msg['From'] = From
+  msg['To'] = To
+  msg['Subject'] = 'Olex2 PythonError Log'
+  body = MIMEText('Please find attached my python log') #Here is the body
+  filename = 'PythonError.log'
+  path = ErrorLogPath + filename
+  
+# Mailing stuff
+  ctype, encoding = mimetypes.guess_type(path)
+  if ctype is None or encoding is not None:
+    ctype = 'application/octet-stream'
+    maintype, subtype = ctype.split('/', 1)
+    fp = open(path, 'rb')
+    print maintype, subtype
+    attach = MIMEBase(maintype, subtype)
+    attach.set_payload(fp.read())
+    encode_base64(attach)
+    fp.close
+    attach.add_header('Content-Disposition', 'attachment', filename=filename)
+
+  msg.attach(attach) #We create our message both attachment and the body
+  msg.attach(body)
+  #print msg.as_string()
+  server = smtplib.SMTP(mailserver)
+  server.sendmail(From, To, msg.as_string()) #Send away
+  server.quit()
+  
+OV.registerFunction(OlexMail)
