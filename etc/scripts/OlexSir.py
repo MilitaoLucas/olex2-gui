c4aaf9ea5f6a2526dd31374fdec2385544021617
xrayman <xrayman>
2009-12-10 14:35:59 +0000

    * Updates
    git-svn-id: http://svn.olex2.org/olex2-gui/trunk@2876 bceb41a0-0b25-4981-bef4-7ef80ce4c728

--- etc/scripts/OlexSir.py
+++ etc/scripts/OlexSir.py
@@ -8,10 +8,10 @@ from olexFunctions import OlexFunctions
 OV = OlexFunctions()
 
 '''
-To run this example script, type spy.example("Hello") in Olex2
+To run this example script, type spy.OlexSir() in Olex2
 '''
 
-def OlexSir(ManAtomContents="C 20 H 20 O 10"):
+def OlexSir(level="normal", ManAtomContents="C 20 H 20 O 10", rhomax="0.25"):
   print "This script takes an exsiting file and pushes it into SIR"
   # Experimenting with patterson function but having issues using the partial command to work?
   patterson = 0
@@ -20,17 +20,17 @@ def OlexSir(ManAtomContents="C 20 H 20 O 10"):
   # In fact probably won't need this as I only need Cell, SpaceGroup, Content (SFAC/UNIT), reflection)
   """
   SIR97 - Altomare A., Burla M.C., Camalli M., Cascarano G.L., Giacovazzo C. , Guagliardi A., Moliterni A.G.G., Polidori G.,Spagna R. (1999) J. Appl. Cryst. 32, 115-119.
-
-  %Data
-  Cell a b c alpha, beta, gamma
-  SpaceGroup NAME
-  Content ATOM Number
-  Reflections filename.hkl
-  %Continue
   """
-  
+ 
+  SirLevel = {
+  'normal' : '%invariants\n%phase\n%fourier\n',
+  'hard' : '%invariants\n%phase\n\tMinfom 0.9\n\tRandom\n%fourier\n',
+  'harder' : '%invariants\n\tCochran\n%phase\n\tRandom\n%fourier\n',
+  'difficult' : '%invariants\n\tCochran\n%phase\n\tSeed 9437\n\tRandom 250\n%fourier\n',
+  'bonkers' : '%invariants\n\tCochran\n%phase\n\tSeed 8793\n\tRandom 500\n%fourier\n'  
+  }
   sirversion = 97
-  #names = OV.FileName()
+  rhomax = 0.250 #default for sintheta/lambda 0.5
   Olex2SirIn = OV.FileName()
   SirCompatCell = ''.join(olx.xf_au_GetCell().split(','))
   SirCompatSymm = ' '.join(re.split("([A-Za-z])", olx.xf_au_GetCellSymm()))
@@ -39,7 +39,8 @@ def OlexSir(ManAtomContents="C 20 H 20 O 10"):
   OlexZ = int(olx.xf_au_GetZ())
   NumPeaks = int(OlexZ) * int(olx.xf_au_GetAtomCount())
   CellV = float(olx.xf_au_GetCellVolume())
-  if ManAtomContents != "C 20 H 20 O 10":
+  if ManAtomContents == " " or ManAtomContents != "C 20 H 20 O 10":
+# Ok allowing the manual input of a formula is great but we really need to check it and make sure it is in the correct format, and or take a couple of formats and convert them
     print "Using user input formula", ManAtomContents
     AtomContents = ManAtomContents
   
@@ -50,67 +51,49 @@ def OlexSir(ManAtomContents="C 20 H 20 O 10"):
   print "Sir Friendly Formula", AtomContents
   print "New formula using Z", OlexZ
   print "Number of atoms to look for", NumPeaks, 'or', float(CellV/18.0)
-
 # There is an issue with ever decreasing returns from Olex2 and SIR.
 # Basically Olex2 is updating the SFAC from SIR results this then is being posted back to if you try again
 # NET result is you end up with NO ATOMS!
+# With the idea of just taking atoms from the produced INS we can get around this
 
 # Creating our SIR file
 # This is primative will need to add features such as patterson on and off
   SIRINS = open("%s.sir"%(Olex2SirIn), 'w')
+  SIRINS.write("%window\n")
   SIRINS.write("%%Structure sir%s\n"%(sirversion))
-  SIRINS.write("%Initialize\n")
+  SIRINS.write("%%job   %s in %s\n"%(Olex2SirIn, olx.xf_au_GetCellSymm()))
+  SIRINS.write("%Init\n")
   SIRINS.write("%Data\n")
   SIRINS.write("\tCell %s\n"%(SirCompatCell))
   SIRINS.write("\tSpaceGroup %s\n"%(SirCompatSymm))
   SIRINS.write("\tContent %s\n"%(AtomContents))
+  SIRINS.write("\tRhomax %s\n"%(rhomax))    
   SIRINS.write("\tReflections %s.hkl\n"%(Olex2SirIn))
-  if patterson > 0:
-    SIRINS.write("%Patterson\n")
-    SIRINS.write("\tF**2\n")
-    SIRINS.write("\tPEAKS %s\n"%(int(CellV/18.0)))
-  else:
-    SIRINS.write("%Continue\n%SHELX\n%END")
+  SIRINS.write("\tFosquare\n")
+  SIRINS.write("%normal\n")
+  SIRINS.write("%s"%(SirLevel[level]))
+  SIRINS.write("%export\n")
+  SIRINS.write("\tshelx  sir%s.res\n"%(sirversion))         
+  SIRINS.write("%end\n")
   SIRINS.close()
   
 # All this need error control
   content = os.popen("sir%s %s.sir "%(sirversion, Olex2SirIn)).read() # This pipes our new .sir file into sir using sirversion can use 92/97 etc
   print content # Output from pipe need proper error control here
-  if patterson > 0:
-    SIRINS = open("%s.sir"%(Olex2SirIn), 'w')
-    SIRINS.write("%%Structure sir%s\n"%(sirversion))
-    SIRINS.write("%Initialize\n")
-    SIRINS.write("%Data\n")
-    SIRINS.write("\tCell %s\n"%(SirCompatCell))
-    SIRINS.write("\tSpaceGroup %s\n"%(SirCompatSymm))
-    SIRINS.write("\tContent %s\n"%(AtomContents))
-    SIRINS.write("\tReflections %s.hkl\n"%(Olex2SirIn))
-    SIRINS.write("%NORMAL\n")
-    SIRINS.write("\tPARTIAL\n")
-    SIROUT = open("%s.out"%(Olex2SirIn), 'r')
-    counter = 0
-    heavy = re.split("([A-Za-z]*)",AtomContents)[-2]
-    print "Heavy = ", heavy
-    for sirline in SIROUT:
-      #print sirline
-      if '  peak  height' in sirline:
-        break
-    for sirline in SIROUT:
-      SIRINS.write("\t%s"%re.sub("^    [1-3]       [0-9]", "%s"%(heavy), sirline))
-      if counter > 1:
-        break
-      counter = counter + 1
-    SIRINS.write("%Continue\n%SHELX\n%END")
-    SIRINS.close()
-    content = os.popen("sir%s %s.sir "%(sirversion, Olex2SirIn)).read() # This pipes our new .sir file into sir using sirversion can use 92/97 etc
-    print content # Output from pipe need proper error control here
-# File generated from SIR - name controlled by the Structure line in .sir file - made sir to prevent overwrite of INS
 
-  src = "sir%s.ins"%sirversion
+# File generated from SIR - name controlled by the Structure line in .sir file - made sir to prevent overwrite of INS
+  src = "sir%s.res"%sirversion
   
 # We are going to push our sir result into our res file
   dst = Olex2SirIn + '.res'
 
+# This needs rewriting to reform the orginal Information in the INS/RES file
+# We have lost the:
+# cell ESD parameters
+# the HKL file used
+# the orginal title
+# This should all be possible to be either prestripped saved and returned
+# This needs to be done before the replacement. Or we should just replace the atoms assuming SIR puts the list in the same order?
 # This works fine in Linux not certain in windows?
   shutil.copy2(src, dst)
 
@@ -119,4 +102,6 @@ def OlexSir(ManAtomContents="C 20 H 20 O 10"):
 
 OV.registerFunction(OlexSir)
 
+  
+  
 
