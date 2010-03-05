eac600f902549c30eaafa73b8108d683734798d6
xrayman <xrayman>
2010-03-05 12:56:55 +0000

    * Now checks for cif and creates if missing * saves to html file and opens automatically * makes both tea and coffee in the morning
    git-svn-id: http://svn.olex2.org/olex2-gui/trunk@2963 bceb41a0-0b25-4981-bef4-7ef80ce4c728

--- etc/scripts/OlexCheckCIF.py
+++ etc/scripts/OlexCheckCIF.py
@@ -1,16 +1,10 @@
 """
-<form method="post" enctype="multipart/form-data"
-action="http://vm02.iucr.org/cgi-bin/checkcif.pl">File name:<br>
-<input type="file" name="file" size="35">
-<input type="hidden" name="runtype" checked="checked" value="symmonly">
-<input type="hidden" name="referer" checked="checked" value="checkcif_server">
-<br>
-<input type="submit" name="UPLOAD" value="Send CIF for checking">
-<p>Select form of checkCIF report</p>
-
-<input type="radio" name="outputtype" value="HTML" checked="checked">HTML
-<input type="radio" name="outputtype" value="PDF">PDF
-</form>
+POST method
+server = "http://vm02.iucr.org/cgi-bin/checkcif.pl"
+"file" is the openned buffered file ideally with enctype="multipart/form-data"
+"runtype" is "symmonly"
+"referer" is "checkcif_server"
+outputtype can be HTML or PDF
 """
 import os
 import sys
@@ -24,19 +18,40 @@ from olexFunctions import OlexFunctions
 OV = OlexFunctions()
 
 '''
-To run this script, type spy.OlexPlaton("help") in Olex2
+To run this script, type spy.OlexCheckCIF() in Olex2
 '''
 
 def OlexCheckCIF():
-  print "You are running flag"
-  filename = open('%s/%s.cif' %(OV.FilePath(), OV.FileName()))
-  #filename = open("%s"%("/home/xray/sucrose.cif"))
+  
+  """if result_filetype >= 1:
+    result_filetype = "pdf"
+  else:
+    result_filetype = "html"
+  """
+  # Only getting html return from IUCr may need to check on this?
+  try:
+    filename = open('%s/%s.cif' %(OV.FilePath(), OV.FileName()))
+  except IOError:
+    print "The file does not exist, creating CIF file"
+    OV.cmd("addins acta")
+    OV.cmd("refine 4")
+    return OlexCheckCIF()
+  result_filetype = "html"
   params = {
                              "runtype": "symmonly",
                              "referer": "checkcif_server",
-                             "outputtype": "html",
+                             "outputtype": result_filetype,
                              "file": filename
   }
+  #print params #Parameter check
   f = urllib2.urlopen("http://vm02.iucr.org/cgi-bin/checkcif.pl", params)
-  print f.read()
+  cifcheck_res = open('%s/%s_checkcif_report.%s' %(OV.FilePath(), OV.FileName(), result_filetype), 'w')
+  cifcheck_res.write("%s"%f.read())
+  cifcheck_res.close()
+  # Now outputs into a html file then opens browser.
+  olx.Shell('%s/%s_checkcif_report.%s' %(OV.FilePath(), OV.FileName(), result_filetype)) # Thanks to Richard G. for this
+  
+  # Need to build a PDF version as well
+  print "Completed Check your webrowser"
+  
 OV.registerFunction(OlexCheckCIF)
