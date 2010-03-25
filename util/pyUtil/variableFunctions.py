MilitaoLucas/olex2-gui

commit 61f3993873aee8527b396e46575ae313211b5a59

Move gui params to separate file, 'gui.params'; separate phil_handler for gui params; retire old vvd code
git-svn-id: http://svn.olex2.org/olex2-gui/trunk@2937 bceb41a0-0b25-4981-bef4-7ef80ce4c728

util/pyUtil/variableFunctions.py
@@ -17,102 +17,11 @@ import variableDefinitions
 import olexex
 from cStringIO import StringIO
 
-initialisingVariables = False
-
 import phil_interface
 
 import iotbx.phil
 import libtbx.phil.command_line
 
-class VVD:
-  def __init__(self):
-    self.user = {}
-    self.gui = {}
-    self.history = {}
-    self.refinement = {}
-    self.metacif = {}
-    self.dimas = {}
-    self.report = {}
-    self.solution = {}
-    self.auto = {}
-    self.workflow = {}
-    self.olex2 = {}
-
-def InitialiseVariables(arg):
-  """Called on startup by macro.
-  
-  Initialises all the variables and objects needed in olex.
-  Should only be called once, on startup.
-  """
-  
-  global initialisingVariables
-  initialisingVariables = True
-  
-  if arg == 'startup':
-    global vvdItems
-    vvdItems = VVD()
-    
-    variables = {
-      'FileName':'none',
-      'FilePath':'none',
-    }
-    if OV.HasGUI():
-      variables.update(variableDefinitions.guiVariables())
-    for varName, value in variables.items():
-      if not OV.IsVar(varName):
-        OV.SetVar(varName,value)
-      if 'gui' in varName:
-        vvdItems.gui.setdefault(varName)
-      if 'olex2' in varName:
-        vvdItems.olex2.setdefault(varName)
-        
-    OV.SetVar('cbtn_solve_on','false')
-    OV.SetVar('cbtn_refine_on','false')
-    OV.SetVar('cbtn_report_on','false')
-    
-    ## copy sample directory to datadir
-    svn_samples_directory = '%s/sample_data' %OV.BaseDir()
-    user_samples_directory = '%s/samples' %OV.DataDir()
-    if os.path.exists(user_samples_directory):
-      OV.SetVar('sample_dir',user_samples_directory)
-    else: 
-      os.mkdir(user_samples_directory)
-
-    if sys.version_info[0] >= 2 and sys.version_info[1] >=6:
-      ignore_patterns = shutil.ignore_patterns('*.svn')
-    else:
-      ignore_patterns = None # back compatiblity for python < 2.6
-
-    samples = os.listdir(svn_samples_directory)
-    for sample in samples:
-      if sample == '.svn': continue
-      if not os.path.exists('%s/%s' %(user_samples_directory,sample)):
-        try:
-          dirname1 = '%s/%s' %(svn_samples_directory,sample)
-          dirname2 = '%s/%s' %(user_samples_directory,sample)
-          if ignore_patterns is not None:
-            shutil.copytree(dirname1, dirname2, ignore=ignore_patterns)
-          else:
-            shutil.copytree(dirname1, dirname2)
-          OV.SetVar('sample_dir','%s/samples' %OV.DataDir())
-        except:
-          pass
-      else:
-        continue
-       
-    ## initialise userDictionaries objects
-    if not userDictionaries.people:
-      userDictionaries.People()
-    if not userDictionaries.localList:
-      userDictionaries.LocalList()
-      
-  elif arg == 'reap':
-    """List of variables (usually structure-specific variables) to be initialised on reap."""
-    
-    ExternalPrgParameters.definedControls = [] # reset defined controls
-    
-  return ""
-OV.registerFunction(InitialiseVariables)
 
 def getOlex2VersionInfo():
   txt = 'Olex2, Durham University (compiled %s)' %OV.GetCompilationInfo()
@@ -137,44 +46,6 @@ def getDefaultPrgMethod(prgType):
       defaultMethod = olexex.sortDefaultMethod(program)
       break
   return defaultPrg, defaultMethod
-
-def getVVD(what_do_i_want='structure'):
-  """ if what_do_i_want is blank, will return the whole vvd """
-  vvd = {}
-  dictionary = {}
-  if what_do_i_want == 'structure':
-    listItems = ['refinement','dimas','metacif','history','report','solution','auto']
-  elif what_do_i_want == 'user':
-    listItems = ['refinement','solution']
-  #elif what_do_i_want == 'auto' or what_do_i_want == 'snum_auto':
-    #listItems = [what_do_i_want]
-  elif what_do_i_want == 'alert':
-    listItems = ['alert']
-  else:
-    listItems = [what_do_i_want]
-    
-  if listItems:
-    for item in listItems:
-      #if what_do_i_want in ('user','auto','alert'):
-      if 'user' in what_do_i_want:
-        dictionary = getattr(user_vvdItems,item.split('_')[-1])
-      else:
-        dictionary = getattr(vvdItems,item)
-      for item in dictionary.keys():
-        try:
-          value = OV.FindValue(item)
-          vvd[item] = value
-        except:
-          continue
-  else:
-    dictionary = getattr(vvdItems,what_do_i_want)
-    for item in dictionary.keys():
-      try:
-        value = OV.FindValue(item)
-        vvd[item] = value
-      except:
-        continue
-  return vvd
 
 def Pickle(item,path):
   if "none/.olex" in path:
@@ -211,15 +82,6 @@ def SwitchAllAlertsOn():
     OV.SetParam(item,'Y')
   SaveUserParams()
 OV.registerFunction(SwitchAllAlertsOn)
-
-def StoreParameters(type=""):
-  if type:
-    d = getVVD(type)
-    for var, value in d.items():
-      olex.m('storeparam %s %s' %(var, value))
-  else:
-    print "Please provide the type of variable you want to save (e.g. gui, snum)"
-OV.registerFunction(StoreParameters)
 
 def VVD_to_phil():
   phil_strings = []
@@ -267,6 +129,7 @@ def get_user_phil():
     return None
 
 def LoadParams():
+  # snum params
   master_phil = phil_interface.parse(file_name="%s/params.phil" %OV.BaseDir())
   phil_handler = phil_interface.phil_handler(
     master_phil=master_phil,
@@ -275,6 +138,12 @@ def LoadParams():
   if user_phil:
     phil_handler.update(phil_file=user_phil)
   olx.phil_handler = phil_handler
+  # gui params
+  master_gui_phil = phil_interface.parse(file_name="%s/gui.params" %OV.BaseDir())
+  gui_phil_handler = phil_interface.phil_handler(
+    master_phil=master_gui_phil,
+    parse=phil_interface.parse)
+  olx.gui_phil_handler = gui_phil_handler
 OV.registerFunction(LoadParams)
 
 def LoadStructureParams():
@@ -293,7 +162,7 @@ snum {
   image.bitmap.name = "%s"
   report.image = "%s/screenshot.png"
   }
-""" %(refinementPrg, refinementMethod, solutionPrg, solutionMethod, 
+""" %(refinementPrg, refinementMethod, solutionPrg, solutionMethod,
       OV.FileName(), OV.FileName(), OV.FileName(), OV.FilePath())
   olx.phil_handler.update(phil_string=snum_phil)
   structure_phil_path = "%s/.olex/%s.phil" %(OV.FilePath(), OV.FileName())

gui.params
@@ -1,0 +1,378 @@
+#############################################################
+# The GUI section. This part is currently under development #
+# Expect frequent changes!                                  #
+#############################################################
+
+gui {
+  base_colour = #6464a0
+    .type = colour
+  logo_colour = #6464a0
+    .type = colour
+  image_font_name = Vera
+    .type = str
+  purple = #a245a2
+    .type = colour
+  red = #ff0000
+    .type = colour
+  orange = #ff8f00
+    .type = colour
+  green = #018F1E
+    .type = colour
+  grey = #555555
+    .type = colour
+
+  xgrid {
+    positive_surface_material = '245;3623911424;3632300160;1.000,1.000,1.000,0.500;1.000,1.000,1.000,1.000;36;36'
+      .type = str
+    negative_surface_material = '245;3628089472;3632300160;1.000,1.000,1.000,0.500;1.000,1.000,1.000,1.000;36;366'
+      .type = str
+  }
+
+  skin {
+    name = default
+      .type = str
+    extension = None
+      .type = str
+    logo_name= None
+      .type = path
+    size_factor = 1.0
+      .type = float
+  }
+
+  html {
+    base_colour = #6464a0
+      .type = colour
+    bg_colour = #efefef
+      .type = colour
+      .caption = "Background colour"
+    font_colour = #6f6f8b
+      .type = colour
+    font_name = Arial
+      .type = str
+    font_size = 2
+      .type = int
+    highlight_colour = #ff7800
+      .type = colour
+
+    input_bg_colour = #F3F3F3
+      .type = colour
+
+    input_height = 18
+      .type = int
+    checkbox_height = 18
+      .type = int
+    checkbox_width = 18
+      .type = int
+    combo_width_2 = 50
+      .type = int
+      .help = Width of a combobox that is to take 2 digits and a decimal point.
+    icon_size = 25
+      .type = int
+    link_colour = #6f6f8b
+      .type = colour
+    table_bg_colour = #F3F3F3
+      .type = colour
+    table_firstcol_colour = #e9e9e9
+      .type = colour
+    table_firstcol_width = 8
+      .type = int
+    MainToolbarTabButtonActive = None
+      .type = str
+    code {
+      bg_colour = #e9e9e9
+        .type = colour
+      fg_colour = #6f6f8b
+        .type = colour
+      html_tag = code
+        .type = str
+      }
+  }
+
+  timage{
+    base_colour = None
+      .type = colour
+    grad_colour = None
+      .type = colour
+    font_colour = None
+      .type = colour
+    grad_step = 0.2
+      .type = float
+    bg_colour_L = 1.6
+      .type = float
+    grad_colour_L = 1.8
+      .type = float
+    font_colour_L = 0.8
+      .type = float
+    font_name = Vera
+      .type = str
+
+    snumtitle{
+      base_colour = None
+        .type = colour
+      grad_colour = None
+        .type = colour
+      height = 28
+        .type = int
+      font_colour = None
+        .type = colour
+      font_name = Vera Bold
+        .type = str
+      font_colour_L = 1.6
+        .type = float
+      font_size = 22
+        .type = int
+      top = 1
+        .type = int
+      left = 5
+        .type = int
+      bg_colour_L = 1.6
+        .type = float
+      grad_step = 0.2
+        .type = float
+      grad_colour_L = 1.0
+        .type = float
+      filefullinfo_size = 10
+        .type = int
+      filefullinfo_colour = None
+        .type = colour
+      filefullinfo_colour_L = 1.5
+        .type = float
+      sginfo_scale = 1.0
+        .type = float
+      corner_rad = 3
+        .type = int
+      rounded = 1100
+        .type = str
+    }
+
+    tab{
+      base_colour = None
+        .type = colour
+      grad_colour = None
+        .type = colour
+      font_colour = None
+        .type = colour
+      height = 25
+        .type = int
+      corner_rad = 5
+        .type = int
+
+      grad_step = 0.3
+        .type = float
+
+      bg_colour_L = 1.5
+        .type = float
+      grad_colour_L = 1.1
+        .type = float
+
+      font_colour_L = 1.8
+        .type = float
+      font_name = Vera Bold
+        .type = str
+      font_size = 14
+        .type = int
+      top = 7
+        .type = int
+      left = 8
+        .type = int
+      rounded = 0100
+        .type = str
+      arrows = triangle:left
+        .type = str
+      off_L = 1.6
+        .type = float
+    }
+
+    cbtn{
+      base_colour = None
+        .type = colour
+      grad_colour = None
+        .type = colour
+      font_colour = None
+        .type = colour
+      height = 20
+        .type = int
+      corner_rad = 1
+        .type = int
+      grad_step = 0.1
+        .type = float
+      bg_colour_L = 1.6
+        .type = float
+      grad_colour_L = 1.8
+        .type = float
+      font_colour_L = 0.8
+        .type = float
+      font_name = Vera Bold
+        .type = str
+      font_size = 14
+        .type = int
+      top = 2
+        .type = int
+      left = 3
+        .type = int
+      rounded = 0101
+        .type = str
+      arrows = "bar:right:20,arrow_right:onup:offdown"
+        .type = str
+      off_L = 1.6
+        .type = float
+    }
+
+    button{
+      base_colour = #ababab
+        .type = colour
+      grad_colour = #dedede
+        .type = colour
+      grad_step = 0.4
+        .type = float
+      height = 16
+        .type = int
+      corner_rad = 3
+        .type = int
+      rounded = 1111
+        .type = str
+      font_size = 12
+        .type = int
+      halign = left
+        .type = str
+      top = 1
+        .type = int
+      left = 4
+        .type = int
+      bg_colour_L = 1.6
+        .type = float
+      grad_colour_L = 1.8
+        .type = float
+      font_colour_L = 0.8
+        .type = float
+      font_colour = None
+        .type = colour
+      arrows = None
+        .type = str
+    }
+
+    h1{
+      base_colour = None
+        .type = colour
+      grad_colour = None
+        .type = colour
+      grad_step = 0.2
+        .type = float
+      height = 16
+        .type = int
+      corner_rad = 3
+        .type = int
+      rounded = 1111
+        .type = str
+      font_size = None
+        .type = int
+      top = 1
+        .type = int
+      left = 16
+        .type = int
+      bg_colour_L = 1.7
+        .type = float
+      grad_colour_L = 1.7
+        .type = float
+      font_colour_L = 0.6
+        .type = float
+      font_colour = None
+        .type = colour
+      arrows = 'arrow'
+        .type = str
+      off_L = 1.2
+        .type = float
+      shadow = True
+        .type = bool
+    }
+
+    h3{
+      height = 16
+        .type = int
+
+      base_colour = None
+        .type = colour
+      grad_colour = None
+        .type = colour
+      font_colour = None
+        .type = colour
+
+      bg_colour_L = 1.3
+        .type = float
+      grad_colour_L = 1.75
+        .type = float
+      font_colour_L = 0.8
+        .type = float
+
+      grad_step = 0.1
+        .type = float
+      corner_rad = 2
+        .type = int
+      rounded = 1111
+        .type = str
+      font_size = None
+        .type = int
+      top = 2
+        .type = int
+      left = 12
+        .type = int
+      arrows = 'arrow'
+        .type = str
+      off_L = 1.2
+        .type = float
+      shadow = False
+        .type = bool
+    }
+  }
+
+  button_colouring = #ababab
+    .type = colour
+  button_writing = #00ff00
+    .type = colour
+  button_font_name = Vera
+    .type = str
+  tab_colour = #ff0000
+    .type = colour
+  tab_font_colour = #ff0000
+    .type = colour
+  infobox_text = '2053;2143326838;2143979345'
+    .type = str
+  infobox_plane = '2053;2136422952;2137144606'
+    .type = str
+  htmlpanelwidth = 400
+    .type = int
+  htmlpanelwidth_margin_adjust = 22
+    .type = int
+  grad_top_left = #05053c
+    .type = colour
+  grad_top_right = #05053c
+    .type = colour
+  grad_bottom_left = #ffffff
+    .type = colour
+  grad_bottom_right = #ffffff
+    .type = colour
+  use_fader=False
+    .type = bool
+  shelx_restraints = None
+    .type = strings
+  shelx_constraints = None
+    .type = strings
+  archive_last = None
+    .type = strings
+
+  help_box{
+    width = 450
+      .type = int
+      .help = Width of the help popup boxes
+    height_factor = 120
+      .type = int
+      .help = A factor that helps estimating the height of the help box by the total number of characters in the help text
+    height_constant = 100
+      .type = int
+      .help = Number of pixels to add to the height of a help box
+    height_max = 500
+      .type = int
+      .help = Maximum height of the help popup boxes
+
+    }
+}

params.phil
@@ -1,383 +1,4 @@
 ##############################################################################
-# The GUI section. This part is currently under development; expect frequent changes!.
-####################################################################
-
-
-gui {
-  base_colour = #6464a0
-    .type = colour
-  logo_colour = #6464a0
-    .type = colour
-  image_font_name = Vera
-    .type = str
-  purple = #a245a2
-    .type = colour
-  red = #ff0000
-    .type = colour
-  orange = #ff8f00
-    .type = colour
-  green = #018F1E
-    .type = colour
-  grey = #555555
-    .type = colour
-    
-  xgrid {
-    positive_surface_material = '245;3623911424;3632300160;1.000,1.000,1.000,0.500;1.000,1.000,1.000,1.000;36;36'
-      .type = str
-    negative_surface_material = '245;3628089472;3632300160;1.000,1.000,1.000,0.500;1.000,1.000,1.000,1.000;36;366'
-      .type = str
-  }  
-
-  skin {
-    name = default
-      .type = str
-    extension = None
-      .type = str
-    logo_name= None
-      .type = path
-    size_factor = 1.0
-      .type = float
-  }
-
-  html {
-    base_colour = #6464a0
-      .type = colour
-    bg_colour = #efefef
-      .type = colour
-      .caption = "Background colour"
-    font_colour = #6f6f8b
-      .type = colour
-    font_name = Arial
-      .type = str
-    font_size = 2
-      .type = int
-    highlight_colour = #ff7800
-      .type = colour
-      
-    input_bg_colour = #F3F3F3
-      .type = colour
-      
-    input_height = 18
-      .type = int
-    checkbox_height = 18
-      .type = int
-    checkbox_width = 18
-      .type = int
-    combo_width_2 = 50
-      .type = int
-      .help = Width of a combobox that is to take 2 digits and a decimal point.
-    icon_size = 25
-      .type = int
-    link_colour = #6f6f8b
-      .type = colour
-    table_bg_colour = #F3F3F3
-      .type = colour
-    table_firstcol_colour = #e9e9e9
-      .type = colour
-    table_firstcol_width = 8
-      .type = int
-    MainToolbarTabButtonActive = None
-      .type = str
-    code {
-      bg_colour = #e9e9e9
-        .type = colour
-      fg_colour = #6f6f8b
-        .type = colour
-      html_tag = code
-        .type = str
-      }
-  }
-
-  timage{
-    base_colour = None
-      .type = colour
-    grad_colour = None
-      .type = colour
-    font_colour = None
-      .type = colour
-    grad_step = 0.2
-      .type = float
-    bg_colour_L = 1.6
-      .type = float
-    grad_colour_L = 1.8
-      .type = float
-    font_colour_L = 0.8
-      .type = float
-    font_name = Vera
-      .type = str
-
-    snumtitle{
-      base_colour = None
-        .type = colour
-      grad_colour = None
-        .type = colour
-      height = 28
-        .type = int
-      font_colour = None
-        .type = colour
-      font_name = Vera Bold
-        .type = str
-      font_colour_L = 1.6
-        .type = float
-      font_size = 22
-        .type = int
-      top = 1
-        .type = int
-      left = 5
-        .type = int
-      bg_colour_L = 1.6
-        .type = float
-      grad_step = 0.2
-        .type = float
-      grad_colour_L = 1.0
-        .type = float
-      filefullinfo_size = 10
-        .type = int
-      filefullinfo_colour = None
-        .type = colour
-      filefullinfo_colour_L = 1.5
-        .type = float
-      sginfo_scale = 1.0
-        .type = float
-      corner_rad = 3
-        .type = int
-      rounded = 1100
-        .type = str
-    }
-
-    tab{
-      base_colour = None
-        .type = colour
-      grad_colour = None
-        .type = colour
-      font_colour = None
-        .type = colour
-      height = 25
-        .type = int
-      corner_rad = 5
-        .type = int
-
-      grad_step = 0.3
-        .type = float
-
-      bg_colour_L = 1.5
-        .type = float
-      grad_colour_L = 1.1
-        .type = float
-
-      font_colour_L = 1.8
-        .type = float
-      font_name = Vera Bold
-        .type = str
-      font_size = 14
-        .type = int
-      top = 7
-        .type = int
-      left = 8
-        .type = int
-      rounded = 0100
-        .type = str
-      arrows = triangle:left
-        .type = str
-      off_L = 1.6
-        .type = float
-    }
-
-    cbtn{
-      base_colour = None
-        .type = colour
-      grad_colour = None
-        .type = colour
-      font_colour = None
-        .type = colour
-      height = 20
-        .type = int
-      corner_rad = 1
-        .type = int
-      grad_step = 0.1
-        .type = float
-      bg_colour_L = 1.6
-        .type = float
-      grad_colour_L = 1.8
-        .type = float
-      font_colour_L = 0.8
-        .type = float
-      font_name = Vera Bold
-        .type = str
-      font_size = 14
-        .type = int
-      top = 2
-        .type = int
-      left = 3
-        .type = int
-      rounded = 0101
-        .type = str
-      arrows = "bar:right:20,arrow_right:onup:offdown"
-        .type = str
-      off_L = 1.6
-        .type = float
-    }
-
-    button{
-      base_colour = #ababab
-        .type = colour
-      grad_colour = #dedede
-        .type = colour
-      grad_step = 0.4
-        .type = float
-      height = 16
-        .type = int
-      corner_rad = 3
-        .type = int
-      rounded = 1111
-        .type = str
-      font_size = 12
-        .type = int
-      halign = left
-        .type = str
-      top = 1
-        .type = int
-      left = 4
-        .type = int
-      bg_colour_L = 1.6
-        .type = float
-      grad_colour_L = 1.8
-        .type = float
-      font_colour_L = 0.8
-        .type = float
-      font_colour = None
-        .type = colour
-      arrows = None
-        .type = str
-    }
-
-    h1{
-      base_colour = None
-        .type = colour
-      grad_colour = None
-        .type = colour
-      grad_step = 0.2
-        .type = float
-      height = 16
-        .type = int
-      corner_rad = 3
-        .type = int
-      rounded = 1111
-        .type = str
-      font_size = None
-        .type = int
-      top = 1
-        .type = int
-      left = 16
-        .type = int
-      bg_colour_L = 1.7
-        .type = float
-      grad_colour_L = 1.7
-        .type = float
-      font_colour_L = 0.6
-        .type = float
-      font_colour = None
-        .type = colour
-      arrows = 'arrow'
-        .type = str
-      off_L = 1.2
-        .type = float
-      shadow = True
-        .type = bool
-    }
-
-    h3{
-      height = 16
-        .type = int
-
-      base_colour = None
-        .type = colour
-      grad_colour = None
-        .type = colour
-      font_colour = None
-        .type = colour
-
-      bg_colour_L = 1.3
-        .type = float
-      grad_colour_L = 1.75
-        .type = float
-      font_colour_L = 0.8
-        .type = float
-
-      grad_step = 0.1
-        .type = float
-      corner_rad = 2
-        .type = int
-      rounded = 1111
-        .type = str
-      font_size = None
-        .type = int
-      top = 2
-        .type = int
-      left = 12
-        .type = int
-      arrows = 'arrow'
-        .type = str
-      off_L = 1.2
-        .type = float
-      shadow = False
-        .type = bool
-    }
-  }
-
-  button_colouring = #ababab
-    .type = colour
-  button_writing = #00ff00
-    .type = colour
-  button_font_name = Vera
-    .type = str
-  tab_colour = #ff0000
-    .type = colour
-  tab_font_colour = #ff0000
-    .type = colour
-  infobox_text = '2053;2143326838;2143979345'
-    .type = str
-  infobox_plane = '2053;2136422952;2137144606'
-    .type = str
-  htmlpanelwidth = 400
-    .type = int
-  htmlpanelwidth_margin_adjust = 22
-    .type = int
-  grad_top_left = #05053c
-    .type = colour
-  grad_top_right = #05053c
-    .type = colour
-  grad_bottom_left = #ffffff
-    .type = colour
-  grad_bottom_right = #ffffff
-    .type = colour
-  use_fader=False
-    .type = bool
-  shelx_restraints = None
-    .type = strings
-  shelx_constraints = None
-    .type = strings
-  archive_last = None
-    .type = strings
-
-  help_box{
-    width = 450
-      .type = int
-      .help = Width of the help popup boxes
-    height_factor = 120
-      .type = int
-      .help = A factor that helps estimating the height of the help box by the total number of characters in the help text
-    height_constant = 100
-      .type = int
-      .help = Number of pixels to add to the height of a help box
-    height_max = 500
-      .type = int
-      .help = Maximum height of the help popup boxes
-
-    }
-}
-
-##############################################################################
 # The sNum section. Information belonging to the currently loaded structure
 ##############################################################################
 
@@ -973,7 +594,7 @@ programs {
           .type = int
         max_solving_iterations = 500
           .type = int
-        } 
+        }
       }
     }
   }

commit 1d40297b6fe66ef5fc1ed5122efa1221e2fb526b

fix for bug in spy.EditParams() caused by changes in r2937

util/pyUtil/variableFunctions.py
@@ -133,7 +133,7 @@ def get_custom_phil():
     return custom_phil_file
   else:
     return None
-  
+
 def LoadParams():
   # snum params
   master_phil = phil_interface.parse(file_name="%s/params.phil" %OV.BaseDir())
@@ -213,8 +213,12 @@ def SaveUserParams():
 OV.registerFunction(SaveUserParams)
 
 def EditParams(scope_name="", expert_level=0, attributes_level=0):
+  if scope_name.startswith("gui"):
+    handler = olx.gui_phil_handler
+  else:
+    handler = olx.phil_handler
   try:
-    output_phil = olx.phil_handler.get_scope_by_name(scope_name)
+    output_phil = handler.get_scope_by_name(scope_name)
     original_name = output_phil.name
     output_phil.name = scope_name
   except KeyError:
@@ -224,7 +228,7 @@ def EditParams(scope_name="", expert_level=0, attributes_level=0):
     output_phil.show(out=s, expert_level=expert_level, attributes_level=attributes_level)
     input_phil_string = OV.GetUserInput(0, "Edit parameters", s.getvalue())
     if input_phil_string is not None and not input_phil_string == s.getvalue():
-      olx.phil_handler.update(phil_string=str(input_phil_string))
+      handler.update(phil_string=str(input_phil_string))
     else:
       # need to set scope name back to original since scope isn't rebuilt
       output_phil.name = original_name