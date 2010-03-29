0a809213672139a0303095ad6752c247f1e2a2a0
horst <horst@olexsys.org>
2010-03-29 21:12:40 +0000

    -- PLEASE DO NOT CHANGE THIS FILE

--- etc/scripts/customScripts.py
+++ etc/scripts/customScripts.py
@@ -3,8 +3,8 @@ Please use this file to add your own imports. We will not update this file.
 """
 
 import example
-import OlexSir
-import OlexPlaton
-import OlexMail
-import OlexCheckCIF
-import hptools
+#import OlexSir
+#import OlexPlaton
+#import OlexMail
+#import OlexCheckCIF
+#import hptools
