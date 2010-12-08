b2ef3190df38ccd131c3537cd13b9ea37a5e21c1
Richard <Richard>
2010-12-08 20:24:57 +0000

    plot line of best fit on bijvoet_differences_NPP; duplicate pairs and line of best fit for bijvoet_differences_scatter_plot
    git-svn-id: http://svn.olex2.org/olex2-gui/trunk@3384 bceb41a0-0b25-4981-bef4-7ef80ce4c728

--- util/pyUtil/CctbxLib/reflection_statistics.py
+++ util/pyUtil/CctbxLib/reflection_statistics.py
@@ -305,8 +305,16 @@ class bijvoet_differences_scatter_plot(OlexCctbxAdapter):
       if not hasattr(hooft_analysis, 'delta_fo2'):
         return
     self.have_bijvoet_pairs = True
-    self.delta_fo2 = hooft_analysis.delta_fo2
-    self.delta_fc2 = hooft_analysis.delta_fc2
+    self.delta_fo2, minus_fo2 =\
+        hooft_analysis.delta_fo2.generate_bijvoet_mates().hemispheres_acentrics()
+    self.delta_fc2, minus_fc2 =\
+        hooft_analysis.delta_fc2.generate_bijvoet_mates().hemispheres_acentrics()
+    # we want to plot both hemispheres
+    self.delta_fo2.indices().extend(minus_fo2.indices())
+    self.delta_fo2.data().extend(minus_fo2.data() * -1)
+    self.delta_fo2.sigmas().extend(minus_fo2.sigmas())
+    self.delta_fc2.indices().extend(minus_fc2.indices())
+    self.delta_fc2.data().extend(minus_fc2.data() * -1)
     self.indices = self.delta_fo2.indices()
 
   def xy_plot_info(self):
@@ -317,6 +325,9 @@ class bijvoet_differences_scatter_plot(OlexCctbxAdapter):
       r.title += ": " + str(self.info)
     r.x = self.delta_fc2.data()
     r.y = self.delta_fo2.data()
+    fit = flex.linear_regression(r.x, r.y)
+    r.fit_slope = fit.slope()
+    r.fit_y_intercept = fit.y_intercept()
     r.indices = self.indices
     r.xLegend = "delta Fc2"
     r.yLegend = "delta Fo2"
