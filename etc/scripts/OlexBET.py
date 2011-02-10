fc146eb8feea2f8b9a8376ffa9b693f9f7cbe087
xrayman <xrayman>
2011-02-10 17:10:44 +0000

    * Latest version - technically called "what a hack"

--- etc/scripts/OlexBET.py
+++ etc/scripts/OlexBET.py
@@ -35,17 +35,20 @@ def OlexBET(probe="n2", trials="5000"):
   print "This script converts the current model and creates a non_ortho_surface_area input dat, runs non_ortho_surface_area and reports the result"
   # We can assume that the INS name and information can come from Olex2
   probetypes = {
-  'n2' : '0.59',
-  'he' : '1',
-  'co2' : '1'
+    'n2' : '3.681',
+    'he' : '1',
+    'co2' : '1'
   }
-    
+
   Olex2BETIn = OV.FileName()
   BETCompatCell = (olx.xf_au_GetCell().split(','))
   brokensym = list(olx.xf_au_GetCellSymm())
   OlexZ = int(olx.xf_au_GetZ())
   AtomPairs = olx.xf_GetFormula().split()
   CellV = float(olx.xf_au_GetCellVolume())
+  OV.cmd("pack cell")
+  OV.File("atoms_%s.xyz"%(Olex2BETIn))
+  OV.AtReap("atoms_%s.xyz"%(Olex2BETIn))
   CalDen = ((float(olx.xf_au_GetWeight())*OlexZ)/(CellV*0.60225))
   print AtomPairs
   AtomGroups = []
@@ -61,8 +64,8 @@ def OlexBET(probe="n2", trials="5000"):
   print "ETF", AtomContents
   #AtomContents = ' '.join(re.split("([A-Za-z]*)",olx.xf_GetFormula()))
   snuff = re.split("([A-Za-z]*)",olx.xf_GetFormula())
-  
- 
+
+  # General stuff for the user to see in Olex2
   print "Job name", Olex2BETIn
   print "Unit Cell", BETCompatCell
   print "Olex2 Formula", olx.xf_GetFormula()
@@ -72,6 +75,9 @@ def OlexBET(probe="n2", trials="5000"):
   print "Cell voume", CellV
   print "Calculated density", CalDen
   print "RAddiiii?", olex_core.GetVdWRadii()
+  print "Probes", probetypes
+  for element in olex_core.GetVdWRadii():
+    print "Element: ",element, "Radii: ", olex_core.GetVdWRadii()[element], "Diameter: ", 2*olex_core.GetVdWRadii()[element]
 
 # Write the BET input file
 # This is primative will need to add features such as patterson on and off
@@ -84,10 +90,23 @@ def OlexBET(probe="n2", trials="5000"):
   BETINS.write("%s %s %s\n"%(BETCompatCell[3], BETCompatCell[4], BETCompatCell[5])) #! unit cell angles alpha, beta, gamma
   BETINS.write("%s\n"%(CalDen)) #! crystal density in g / cm3
   BETINS.close()
+
+  ATOMSINS= open("atoms_%s.atoms"%(Olex2BETIn), 'w')
+  for element in olex_core.GetVdWRadii():
+    ATOMSINS.write("%s %s\n"%(element, 2*olex_core.GetVdWRadii()[element])) #! file containing the atom types and diameters
+  ATOMSINS.write("EOF")
+  ATOMSINS.close()
+
   
 # All this need error control
-  content = os.popen("nonorthoSA.exe < %s.dat > %s_BET.log"%(Olex2BETIn, Olex2BETIn)).read() # This pipes our new .dat file into nonortho
-  print content # Output from pipe need proper error control here
+  try:
+    print "Running BET calculation now"
+    content = os.popen("nonorthoSA.exe < %s.dat > %s_BET.log"%(Olex2BETIn, Olex2BETIn)).read() # This pipes our new .dat file into nonortho
+    #BET_result = olx.Exec("nonorthoSA.exe < %s.dat > %s_BET.log"%(Olex2BETIn, Olex2BETIn))
+    print "Finished calculation"
+  except:
+    print "BET calculation failed to run"
+    return
+  #print content # Output from pipe need proper error control here
 
 OV.registerFunction(OlexBET)
-
