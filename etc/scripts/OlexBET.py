81bd13e8a2a48316a1b6becdf2ae8b9add53f8a6
xrayman <xrayman>
2011-02-17 14:12:05 +0000

    * I've gone mad I tell you! I've just typed in all the probe diameters I could find from viscosity

--- etc/scripts/OlexBET.py
+++ etc/scripts/OlexBET.py
@@ -35,12 +35,78 @@ To run type spy.OlexBET() in Olex2
 def OlexBET(probe="n2", trials="5000"):
   print "This script converts the current model and creates a non_ortho_surface_area input dat, runs non_ortho_surface_area and reports the result"
   # We can assume that the INS name and information can come from Olex2
+  # These probe values are from the AIC group standards KS uses 3.4 and 3.2 for N2 and CO2
+  """probetypes = {
+    'n2' : '3.64',
+    'me' : '3.24',
+    'co2' : '3.4',
+    'h2' : '2.84'
+  }"""
+  
+  # These probe types come from:
+  # Hirschfelder, C. F.; Curtiss, C. F.; Bird, R. B. Molecular Theory of Gases and Liquids; Wiley: New York, 1954; p 1110
+  # They are the constants defined by viscosity data and are from the 80 to 300 K temperature range result set.
   probetypes = {
-    'n2' : '3.4',
-    'he' : '1',
-    'co2' : '3.2'
+    # Light Gases
+    'He' : '2.576',
+    'H2': '2.968',
+    'D2' : '2.948',
+    # Noble Gases
+    'Ne' : '2.789',
+    'Ar' : '3.418',
+    'Kr' : '3.61',
+    'Xe' : '4.055',
+    # Simple Polyatomic Gases
+    'Air' : '3.617',
+    'N2' : '3.681',
+    'O2' : '3.433',
+    'CO' : '3.590',
+    'CO2' : '3.996',
+    'NO' : '3.470',
+    'N2O' : '3.879',
+    'CH4' : '3.822',
+    'CCl4' : '5.881',
+    'SO2' : '4.290',
+    'F2' : '3.653',
+    'Cl2' : '4.115',
+    'Br2' : '4.268',
+    'I2' : '4.982',
+    # Other Inorganic Vapours
+    'HCl' : '3.305',
+    'HI' : '4.123',
+    'AsH3' : '4.06',
+    'HgI2' : '5.625',
+    'HgBr2' : '5.414',
+    'SnBr4' : '6.666',
+    'SnCl4' : '4.540',
+    'Hg' : '2.898',
+    # Hydrocarbons
+    'C2H2' : '4.221',
+    'C2H4' : '4.232',
+    'C2H6' : '4.418',
+    'C3H8' : '5.061',
+    'nC4H10' : '4.997',
+    'iC4H10' : '5.341',
+    'nC5H12' : '5.769',
+    'nC6H14' : '5.909',
+    'nC8H18' : '7.451',
+    'nC9H20' : '8.448',
+    'chexane' : '6.093',
+    'C6H6' : '5.270',
+    # Other Organic Vapours
+    'CH3OH' : '3.585',
+    'C2H5OH' : '4.455',
+    'CH3Cl' : '3.375',
+    'CH2Cl2' : '4.759',
+    'CHCl3' : '5.430',
+    'C2N2' : '4.38',
+    'COS' : '4.13',
+    'CS2' : '4.438',
+    # Common Names
+    'DCM' : '4.759',
+    'Me' : '3.822'
   }
-
+  
   Olex2BETIn = OV.FileName()
   BETCompatCell = (olx.xf_au_GetCell().split(','))
   brokensym = list(olx.xf_au_GetCellSymm())
