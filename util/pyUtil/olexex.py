16025a7fdeb97e4fe989769243d6e4649956fee3
horst <horst@olexsys.org>
2011-05-10 21:08:58 +0000

    -- ccdc_upload

--- util/pyUtil/olexex.py
+++ util/pyUtil/olexex.py
@@ -1710,8 +1710,8 @@ def settings_tree():
                   for m1 in m.objects:
                     if m1.is_scope:
                       l.append("\t\t\t\t%s (%s)\n%s.%s.%s.%s.%s\n" %(m1.name, m1.short_caption, item, tem.name, em.name, m.name, m1.name))
-          
-          
+
+
   OV.write_to_olex('settings_tree.ind', ''.join(l))
 OV.registerFunction(settings_tree)
 
@@ -1741,6 +1741,35 @@ def GetTwinLaw(html=False):
   return txt
 OV.registerFunction(GetTwinLaw)
 
+def ccdc_submit():
+  #import urllib2
+  #import urllib2_file
+
+  file_name = os.path.normpath(olx.file_ChangeExt(OV.FileFull(),'cif'))
+  rFile = open(file_name, 'rb')
+  cif = rFile.read()
+
+  url = OV.GetParam('user.ccdc.portal_url')
+  params = {'__ac_password':OV.GetParam('user.ccdc.portal_passwd'),
+            '__ac_name':OV.GetParam('user.ccdc.portal_username'),
+            'context':"None",
+            'cif': cif
+            }
+#  params = urllib.urlencode(params)
+#  f = urllib2.urlopen(url, params)
+
+  try:
+    proxy = get_proxy_from_usettings()
+    proxies = {'http': proxy}
+    proxy_support = urllib2.ProxyHandler(proxies)
+    req = urllib2.Request(url)
+    response = urllib2.urlopen(req,params)
+    f = response.read()
+  except:
+    print('Failed')
+  rFile.close()
+  print f
+OV.registerFunction(ccdc_submit)
 
 def HklStatsAnalysis():
   import olex_core
