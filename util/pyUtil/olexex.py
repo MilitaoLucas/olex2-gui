edb03f50f7c0455530952a2ee5d94ad253596a3b
horst <horst@olexsys.org>
2011-05-31 14:36:26 +0000

    -- added '.' as an allowed item for cif colour intensity/qualifier -- macrox uses shell instead of run when starting explorer -- set sadabs multi-scan before reading the file

--- util/pyUtil/olexex.py
+++ util/pyUtil/olexex.py
@@ -1754,35 +1754,6 @@ def GetTwinLaw(html=False):
   return txt
 OV.registerFunction(GetTwinLaw)
 
-def ccdc_submit():
-  #import urllib2
-  #import urllib2_file
-
-  file_name = os.path.normpath(olx.file_ChangeExt(OV.FileFull(),'cif'))
-  rFile = open(file_name, 'rb')
-  cif = rFile.read()
-
-  url = OV.GetParam('user.ccdc.portal_url')
-  params = {'__ac_password':OV.GetParam('user.ccdc.portal_passwd'),
-            '__ac_name':OV.GetParam('user.ccdc.portal_username'),
-            'context':"None",
-            'cif': cif
-            }
-#  params = urllib.urlencode(params)
-#  f = urllib2.urlopen(url, params)
-
-  try:
-    proxy = get_proxy_from_usettings()
-    proxies = {'http': proxy}
-    proxy_support = urllib2.ProxyHandler(proxies)
-    req = urllib2.Request(url)
-    response = urllib2.urlopen(req,params)
-    f = response.read()
-  except:
-    print('Failed')
-  rFile.close()
-  print f
-OV.registerFunction(ccdc_submit)
 
 def HklStatsAnalysis():
   import olex_core
