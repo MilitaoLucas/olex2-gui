5e20258150b090bb95864c8fe813435ce3b3dd23
horst <horst@olexsys.org>
2011-06-07 14:50:33 +0000

    properties for scripts in /scripts
    git-svn-id: http://svn.olex2.org/olex2-gui/trunk@3630 bceb41a0-0b25-4981-bef4-7ef80ce4c728

--- etc/scripts/userScripts.py
+++ etc/scripts/userScripts.py
@@ -3,12 +3,20 @@ These imports are of user contributed scripts.
 This file WILL be updated by us
 """
 
-import OlexSir
-import OlexPlaton
-import OlexMail
-import OlexCheckCIF
-import OlexCDS
-import flipsmall
-import OlexBET
-import Olexhole
-#import LazyOlex
+scripts = [
+  'OlexPlaton',
+  'OlexMail',
+  'OlexCheckCIF',
+  'OlexCDS',
+  'flipsmall',
+  'OlexBET',
+  'Olexhole',
+  #'OlexSir',
+  #'LazyOlex',
+]
+
+for script in scripts:
+  try:
+    __import__(script)
+  except Exception, ex:
+    print "Import from userScripts.py failed: %s" %ex
