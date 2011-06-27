f58f4e5854b246ffccf84088cde9d29b35658358
horst <horst@olexsys.org>
2011-06-27 17:34:42 +0000

    git-svn-id: http://svn.olex2.org/olex2-gui/trunk@3679 bceb41a0-0b25-4981-bef4-7ef80ce4c728

--- etc/scripts/Olex2CCDC.py
+++ etc/scripts/Olex2CCDC.py
@@ -133,7 +133,7 @@ class CcdcSubmit():
     except Exception, err:
       print('Data submission failed')
       f = err
-    print "The Structure has been submitted to the CCDC."
+    print f
     
 CcdcSubmit_instance = CcdcSubmit()
 OV.registerFunction(CcdcSubmit_instance.ccdc_submit)
