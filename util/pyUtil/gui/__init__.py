e716445ff3acfe94388b2d9f80c9bfaed5538e11
pcxod <oleg_dolomanov@hotmail.com>
2011-08-13 01:41:15 +0000

    making a proper About box

--- util/pyUtil/gui/__init__.py
+++ util/pyUtil/gui/__init__.py
@@ -8,3 +8,18 @@ def FileOpen(title, filter, location, default=''):
   return res
 
 olex.registerFunction(FileOpen, False, "gui.dialog")
+
+def About():
+  sz = [int(i) for i in olx.GetWindowSize().split(',')]
+  w = int(olx.ClientWidth('self'))
+  h = int(olx.ClientHeight('self'))
+  sw = 350
+  sh = 250
+  olx.Popup("about %s/etc/gui/help/about.htm -x=%d -y=%d -w=%d -h=%d"
+            %(olx.BaseDir(),
+              sz[0] + w/2 + sw/2,
+              sz[1] + h/2 - sh/2,
+              sw,
+              sh))
+
+olex.registerFunction(About, False, "gui")
