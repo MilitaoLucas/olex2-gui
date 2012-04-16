e752566fe1017f3f718a97c8b901275940a57e6e
pcxod <oleg_dolomanov@hotmail.com>
2012-04-16 10:56:10 +0000

    making sure that the maps and GUI controls are synchronised - refactoring to separate map logic from the rest;
    git-svn-id: http://svn.olex2.org/olex2-gui/trunk@4190 bceb41a0-0b25-4981-bef4-7ef80ce4c728

--- util/pyUtil/olexex.py
+++ util/pyUtil/olexex.py
@@ -674,17 +674,12 @@ def MakeElementButtonsFromFormula():
   if current_formula != last_formula:
     last_formula = current_formula
 
-
   OV.write_to_olex('element_buttons.htm', html, 0)
-
 
   im_name='IMG_BTN-ELEMENT%s' %symbol
   OV.SetImage(im_name, name)
 
   SetAtomicVolumeInSnumPhil(totalcount)
-
-
-
 
 if haveGUI:
   OV.registerFunction(MakeElementButtonsFromFormula)
@@ -720,131 +715,6 @@ def CheckBoxValue(var, def_val='false'):
   return str(retVal)
 if haveGUI:
   OV.registerFunction(CheckBoxValue)
-
-def VoidView(recalculate='0', onoff=None):
-  img_bases = ['button_small-void']
-  if deal_with_map_buttons(onoff, img_bases, 'void'):
-    return
-  if OV.IsControl('SNUM_MAP_BUTTON'):
-    # set electron density map button to 'up' state
-    olx.SetState('SNUM_MAP_BUTTON','up')
-    olx.SetLabel('SNUM_MAP_BUTTON',OV.Translate('Calculate'))
-
-  resolution = OV.GetParam("snum.calcvoid.resolution")
-  distance = OV.GetParam("snum.calcvoid.distance")
-  precise = OV.GetParam("snum.calcvoid.precise")
-  SetXgridView()
-  cmd = "calcVoid -r=%s -d=%s" %(resolution, distance)
-  if precise:
-    cmd += " -p"
-  olex.m(cmd)
-  OV.SetParam('olex2.void_vis',True)
-
-if haveGUI:
-  OV.registerFunction(VoidView)
-
-def MaskView(onoff=None):
-  img_bases = ['button_small-mask']
-  if deal_with_map_buttons(onoff, img_bases, 'mask'):
-    return
-  SetXgridView()
-  olex.m('spy.OlexCctbxMasks()')
-  OV.SetVar('olex2.mask_vis',True)
-
-if haveGUI:
-  OV.registerFunction(MaskView)
-
-
-
-def MapView(onoff=None):
-  img_bases = ['button_full-electron_density_map', 'button_small-map']
-  if deal_with_map_buttons(onoff, img_bases, 'eden'):
-    return
-
-  if OV.IsControl('SNUM_CALCVOID_BUTTON'):
-    # set calcvoid button to 'up' state
-    olx.SetState('SNUM_CALCVOID_BUTTON','up')
-    olx.SetLabel('SNUM_CALCVOID_BUTTON',OV.Translate('Calculate Voids'))
-
-  map_type =  OV.GetParam("snum.map.type")
-  map_source =  OV.GetParam("snum.map.source")
-  map_resolution = OV.GetParam("snum.map.resolution")
-  mask = OV.GetParam("snum.map.mask")
-
-  if map_type == "fcalc":
-    map_type = "calc"
-  elif map_type == "fobs":
-    map_type = "obs"
-
-  if mask:
-    mask_val = "-m"
-  else:
-    mask_val = ""
-
-  SetXgridView()
-
-  if map_source == "olex":
-    if OV.GetParam("snum.refinement.use_solvent_mask"):
-      modified_hkl = "%s/%s_modified.hkl"
-
-    olex.m("calcFourier -%s -r=%s %s" %(map_type, map_resolution, mask_val))
-  else:
-    olex.m("calcFourier -%s -%s -r=%s %s" %(map_type, map_source, map_resolution, mask_val))
-
-  if olex_gui.IsControl('SNUM_XGRID_SLIDE'):
-    olx.html.SetValue('SNUM_XGRID_SLIDE', '%f,%f'
-                      %(float(olx.xgrid.GetMin())*100, float(olx.xgrid.GetMax())*100))
-  OV.SetVar('olex2.eden_vis',True)
-
-if haveGUI:
-  OV.registerFunction(MapView)
-
-def deal_with_map_buttons(onoff, img_bases, map_type):
-  ## First, set all images to hidden
-  tl = ['eden', 'void', 'mask']
-  for item in tl:
-    if item != map_type:
-      OV.SetParam('olex2.%s_vis' %item, False)
-
-  if not onoff:
-    if OV.GetParam('olex2.%s_vis' %map_type) == False:
-      onoff = 'on'
-    elif OV.GetParam('olex2.%s_vis' %map_type) == True:
-      onoff = 'off'
-
-  if onoff == 'off':
-    OV.SetParam('olex2.%s_vis' %map_type,False)
-    olex.m('xgrid.visible(false)')
-    for img_base in img_bases:
-      use_image= "up=%soff.png" %img_base
-      OV.SetImage("IMG_%s" %img_base.upper(),use_image)
-    retVal = True
-
-  if onoff == 'on':
-    OV.SetParam('olex2.%s_vis' %map_type,True)
-    for img_base in img_bases:
-      use_image= "up=%son.png" %img_base
-      OV.SetImage("IMG_%s" %img_base.upper(),use_image)
-    retVal = False
-  return retVal
-
-def SetXgridView():
-  view = OV.GetParam("snum.xgrid.view")
-  extended = OV.GetParam("snum.xgrid.extended")
-  if view == "2D":
-    olex.m("xgrid.RenderMode(plane)")
-  elif view == "surface":
-    olex.m("xgrid.RenderMode(fill)")
-  elif view == "wire":
-    olex.m("xgrid.RenderMode(line)")
-  elif view == "points":
-    olex.m("xgrid.RenderMode(point)")
-  else:
-    olex.m("xgrid.RenderMode(%s)" %view)
-  olex.m("xgrid.Extended(%s)" %extended)
-
-if haveGUI:
-  OV.registerFunction(SetXgridView)
 
 def GetHklFileList():
   reflection_file_extensions = ["hkl", "hkp", "raw", 'hklf5', 'hkc']
