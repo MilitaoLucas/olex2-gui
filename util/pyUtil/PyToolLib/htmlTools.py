ba56e0154e3a895657c0abd9636afc7b478c6171
pcxod <oleg_dolomanov@hotmail.com>
2012-09-11 16:40:32 +0000

    more standardisation; adding tif to the list of the picture formats;
    git-svn-id: http://svn.olex2.org/olex2-gui/trunk@4341 bceb41a0-0b25-4981-bef4-7ef80ce4c728

--- util/pyUtil/PyToolLib/htmlTools.py
+++ util/pyUtil/PyToolLib/htmlTools.py
@@ -62,10 +62,10 @@ def makeHtmlTable(list):
         box_d.setdefault('ctrl_name', "SET_%s" %str.upper(box_d['varName']).replace('.','_'))
         box_d.setdefault('bgcolor','spy.bgcolor(~name~)')
         if box_d['varName'].startswith('_'): # treat cif items differently
-          box_d.setdefault('value', '$spy.get_cif_item(%(varName)s,?,gui)' %box_d)
+          box_d.setdefault('value', 'spy.get_cif_item(%(varName)s,?,gui)' %box_d)
           box_d.setdefault('onchange',"spy.set_cif_item('%(varName)s',html.GetValue('~name~'))>>spy.changeBoxColour('%(ctrl_name)s','#FFDCDC')" %box_d)
         else:
-          box_d.setdefault('value', "$spy.GetParam('%(varName)s')" %box_d)
+          box_d.setdefault('value', "spy.GetParam('%(varName)s')" %box_d)
           box_d.setdefault('onchange',"spy.SetParam('%(varName)s',html.GetValue('~name~'))>>spy.AddVariableToUserInputList('%(varName)s')>>spy.changeBoxColour('~name~','#FFDCDC')" %box_d)
         boxText += makeHtmlInputBox(box_d)
     if boxText:
@@ -73,19 +73,19 @@ def makeHtmlTable(list):
     else:
       input_d.setdefault('ctrl_name', "SET_%s" %str.upper(input_d['varName']).replace('.','_'))
       if input_d['varName'].startswith('_'): # treat cif items differently
-        input_d.setdefault('value', "$spy.get_cif_item('%(varName)s','?','gui')" %input_d)
+        input_d.setdefault('value', "spy.get_cif_item('%(varName)s','?','gui')" %input_d)
         input_d.setdefault('onchange',"spy.set_cif_item('%(varName)s',html.GetValue('~name~'))>>spy.changeBoxColour('~name~','#FFDCDC')" %input_d)
       elif input_d['varName'] == 'snum.report.date_collected': # treat date fields differently
         try:
           cd = float(OV.GetParam('snum.report.date_collected'))
           cd = date.fromtimestamp(cd)
           time_str = cd.strftime("%d-%m-%Y")
-          input_d.setdefault('value', time_str)
+          input_d.setdefault('value', "'%s'" %time_str)
         except:
-          input_d.setdefault('value', OV.GetParam('snum.report.date_collected'))
+          input_d.setdefault('value', "'%s'" %OV.GetParam('snum.report.date_collected'))
         input_d.setdefault('onchange',"spy.SetParam('%(varName)s',html.GetValue('~name~'))>>spy.AddVariableToUserInputList('%(varName)s')>>spy.changeBoxColour('~name~','#FFDCDC')" %input_d)
       else:
-        input_d.setdefault('value', "$spy.GetParam('%(varName)s')" %input_d)
+        input_d.setdefault('value', "spy.GetParam('%(varName)s')" %input_d)
         input_d.setdefault('onchange',"spy.SetParam('%(varName)s',html.GetValue('~name~'))>>spy.AddVariableToUserInputList('%(varName)s')>>spy.changeBoxColour('~name~','#FFDCDC')" %input_d)
       input_d.setdefault('bgcolor',"spy.bgcolor('~name~')")
       row_d.setdefault('input',makeHtmlInputBox(input_d))
@@ -118,17 +118,17 @@ def makeHtmlInputBox(inputDictionary):
     'bgcolor':'',
   }
   dictionary.update(inputDictionary)
-  value = dictionary.get('value', '')
+
   htmlInputBoxText = '''
-<font size='$GetVar(HtmlFontSizeControls)'>
+<font size="$GetVar('HtmlFontSizeControls')">
 <input
 type="%(type)s"
 %(multiline)s
 width="%(width)s"
 height="%(height)s"
 name="%(ctrl_name)s"
-value="'%(value)s'"
-items="'%(items)s'"
+value="%(value)s"
+items="%(items)s"
 label="%(label)s"
 valign="%(valign)s"
 onchange="%(onchange)s"
