dbae92a4c56ebfbce3454923de133a40b51c63a7
horst <horst@olexsys.org>
2013-07-10 12:51:29 +0000

    crystal image advance fixed, movie starts from current image, moved things to gui.reports from olexex
    git-svn-id: http://svn.olex2.org/olex2-gui/trunk@4515 bceb41a0-0b25-4981-bef4-7ef80ce4c728

--- util/pyUtil/olexex.py
+++ util/pyUtil/olexex.py
@@ -1876,57 +1876,6 @@ def standardise_path(path):
   return OV.standardizePath(path)
 OV.registerFunction(standardise_path)
 
-def play_crystal_images():
-  import time
-  l = OV.GetParam('snum.metacif.list_crystal_images_files')[0].split(';')
-  for image in l:
-    if os.path.exists(image):
-      OV.SetParam('snum.report.crystal_image',OV.standardizePath(image))
-      olx.html.SetImage('CRYSTAL_IMAGE',image)
-      OV.Refresh()
-OV.registerFunction(play_crystal_images)
-
-
-def advance_crystal_image(direction='forward'):
-  l = OV.GetParam('snum.metacif.list_crystal_images_files')[0].split(';')
-  i = 0
-  for image in l:
-    i += 1
-    if image == OV.GetParam('snum.report.crystal_image'):
-      if direction == 'forward':
-        if i != len(l):
-          p = l[i]
-          OV.SetParam('snum.report.crystal_image',p)
-          olx.html.SetImage('CRYSTAL_IMAGE',p)
-          return
-        else:
-          print "Last image of the series!"
-          return
-      else:
-        if i != 1:
-          p = l[i-2]
-          OV.SetParam('snum.report.crystal_image',p)
-          olx.html.SetImage('CRYSTAL_IMAGE',p)
-          return
-        else:
-          print "First image of the series!"
-          return
-    else:
-      continue
-
-  #p = OV.GetParam('snum.report.crystal_image')
-  #n = int(p.split('\\')[-1].split(OV.FileName())[1].split('.')[0])
-  #if direction == 'forward':
-    #n += 1
-  #else:
-    #n -= 1
-  #base = ("\\").join(p.split('\\')[:-1])
-  #p = r"%s\%s%i%s" %(base, OV.FileName(), n, ".jpg")
-  #if os.path.exists(p):
-    #OV.SetParam('snum.report.crystal_image',p)
-    #olx.html.SetImage('CRYSTAL_IMAGE',p)
-OV.registerFunction(advance_crystal_image)
-
 def getCellHTML():
   crystal_systems = {
     'Triclinic':('a', 'b', 'c', '&alpha;', '&beta;', '&gamma;'),
