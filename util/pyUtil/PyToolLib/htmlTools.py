10fcf3b642ae03bc6d94f1a5b2aef1c2bce35577
horst <horst@olexsys.org>
2013-08-07 11:30:53 +0000

    prevent formula from pushing icons over, make smaller font

--- util/pyUtil/PyToolLib/htmlTools.py
+++ util/pyUtil/PyToolLib/htmlTools.py
@@ -24,11 +24,6 @@ last_mode_options = None
 current_tooltip_number = 0
 HaveModeBox = False
 
-global formula
-global formula_string
-formula = ""
-formula_string = ""
-
 global tutorial_box_initialised
 tutorial_box_initialised = False
 
@@ -1556,30 +1551,6 @@ def getGenericSwitchNameTranslation(name):
     text = "No text!"
   return text
 OV.registerFunction(getGenericSwitchNameTranslation)
-
-def makeFormulaForsNumInfo():
-  global formula
-  global formula_string
-
-  if olx.FileName() == "Periodic Table":
-    return "Periodic Table"
-  else:
-    colour = ""
-    txt_formula = olx.xf.GetFormula()
-    if txt_formula == formula:
-      return formula_string
-    formula = txt_formula
-    l = ['3333', '6667']
-    for item in l:
-      if item in txt_formula:
-        colour = OV.GetParam('gui.red').hexadecimal
-    if not colour:
-      colour = OV.GetParam('gui.html.formula_colour').hexadecimal
-    font_size = OV.GetParam('gui.html.formula_size')
-    html_formula = olx.xf.GetFormula('html',1)
-    formula_string = "<font size=%s color=%s>%s</font>" %(font_size, colour, html_formula)
-    return formula_string
-OV.registerFunction(makeFormulaForsNumInfo)
 
 def setDisplayQuality(q=None):
   OV.setDisplayQuality(q)
