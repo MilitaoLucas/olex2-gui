4ed146bf7e373881b14792674f40c2dad7fd71e6
pcxod <oleg_dolomanov@hotmail.com>
2014-07-11 15:28:43 +0000

    ARGH, reverting the wrong commit

--- util/pyUtil/CctbxLib/refinement.py
+++ util/pyUtil/CctbxLib/refinement.py
@@ -431,8 +431,6 @@ class FullMatrixRefine(OlexCctbxAdapter):
         rv = self.twin_components
       else:
         rv += self.twin_components
-    if rv:
-      rv = list(set(rv))
     return rv
 
   def check_flack(self):
