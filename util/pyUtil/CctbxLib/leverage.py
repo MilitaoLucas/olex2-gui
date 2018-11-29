0982158fd5ad7c4f45358f92d5185bafafc23dc9
pcxod <oleg_dolomanov@hotmail.com>
2018-11-29 15:19:20 +0000

    more on leverage - allowing to refine along other twin laws/HKLF5 and automatically adding BASF if needed; updating the output format

--- util/pyUtil/CctbxLib/leverage.py
+++ util/pyUtil/CctbxLib/leverage.py
@@ -12,25 +12,102 @@ from scitbx.lstbx import normal_eqns
 from scitbx.array_family import flex
 from smtbx.structure_factors import direct
 from smtbx.refinement.restraints import origin_fixing_restraints
+from smtbx.refinement import least_squares
 import math
 import sys
 
 import olex
 import olex_core
 import olx
+
+class leverage_normal_eqns(least_squares.crystallographic_ls_class()):
+  log = None
+  def __init__(self, observations, reparametrisation, olx_atoms, **kwds):
+    super(leverage_normal_eqns, self).__init__(
+      observations, reparametrisation, initial_scale_factor=float(olx.xf.rm.OSF()), **kwds)
+
 class Leverage(object):
   def __init__(self):
     pass
 
-  def calculate(self, threshold=0.01):
+  def for_flack(self, max_reflections=5, output_to=None):
     from refinement import FullMatrixRefine
-    dmb = FullMatrixRefine().run(build_only=True)
-    calculate(dmb, float(threshold), None)
+    from cctbx import sgtbx
+    from cctbx.xray import observations
+    from scitbx.lstbx import normal_eqns_solving
 
-  def calculate_for(self, params=""):
+    fmr = FullMatrixRefine()
+    obs = None
+    refine = True
+    # BASF 1 in already?
+    if fmr.twin_components is not None and len(fmr.twin_components):
+      if fmr.twin_components[0].twin_law == sgtbx.rot_mx((-1,0,0,0,-1,0,0,0,-1)):
+        obs = fmr.observations
+        refine = False
+    elif fmr.twin_fractions and len(fmr.twin_fractions):
+      pass
+    else:
+      obs = fmr.observations
+
+    if not obs:
+      fo2 = fmr.reflections.f_sq_obs_filtered
+      fc = fmr.f_calc(None, fmr.exti is not None, True, True)
+      obs = fmr.observations.detwin(
+        fo2.crystal_symmetry().space_group(),
+        fo2.anomalous_flag(),
+        fc.indices(),
+        fc.as_intensity_array().data())
+      obs.fo_sq = fo2
+
+    if refine:
+      it = xray.twin_component(sgtbx.rot_mx((-1,0,0,0,-1,0,0,0,-1)), 0.2, True)
+      fmr.observations = observations.customized_copy(obs, [], (it,))
+      fmr.twin_components = (it,)
+      fmr.twin_fractions = ()
+
+    normal_eqns = fmr.run(build_only=True, normal_equations_class=leverage_normal_eqns)
+
+# need to refine if invertion twin has been added
+    if refine:
+      from libtbx import utils
+      def args(scale_factor, weighting_scheme):
+        args = (normal_eqns,
+                normal_eqns.observations,
+                fmr.f_mask,
+                weighting_scheme,
+                scale_factor,
+                normal_eqns.one_h_linearisation,
+                normal_eqns.reparametrisation.jacobian_transpose_matching_grad_fc(),
+                fmr.exti)
+        return args
+
+      scale_factor = float(olx.xf.rm.OSF())
+      scale_factor *= scale_factor
+      print("Refining the structure...")
+      cycles = normal_eqns_solving.naive_iterations(
+        normal_eqns, n_max_iterations=10,
+        gradient_threshold=1e-7,
+        step_threshold=1e-4)
+      flack = fmr.twin_components[0].value
+      t_vcv = normal_eqns.covariance_matrix(
+        jacobian_transpose=fmr.reparametrisation.jacobian_transpose_matching(
+          fmr.reparametrisation.mapping_to_grad_fc_independent_scalars))
+      su = math.sqrt(t_vcv.matrix_packed_u_diagonal()[0])
+      flack = utils.format_float_with_standard_uncertainty(flack, su)
+      print("Flack parameter has been refined to: %s" %flack)
+      normal_eqns.reset()
+# end of the structure refinement
+    calculate(normal_eqns, None, ["BASF1"], max_reflections=max_reflections, output_to=output_to)
+
+  def calculate(self, threshold=0.01, max_reflections=5):
     from refinement import FullMatrixRefine
     dmb = FullMatrixRefine().run(build_only=True)
-    calculate(dmb, None, params.split(' '))
+    calculate(dmb, float(threshold), None, max_reflections=max_reflections)
+
+  def calculate_for(self, params, max_reflections=5, output_to=None):
+    from refinement import FullMatrixRefine
+    dmb = FullMatrixRefine().run(build_only=True)
+    calculate(dmb, None, params.split(' '), max_reflections=max_reflections, output_to=output_to)
 
   # figure out the crystallographic parameter labels
 def parameter_labels(self, n_params):
@@ -58,7 +135,7 @@ def parameter_labels(self, n_params):
     labels.append(", ".join(label))
   return labels
 
-def calculate(self, threshold, params):
+def calculate(self, threshold, params, max_reflections, output_to):
   import numpy as np
   import scipy.linalg as scla
 
@@ -122,162 +199,51 @@ def calculate(self, threshold, params):
     Ts_.sort(key=lambda x: x[1],reverse=True)
     if Ts_[0][1] > maxT:
       maxT = Ts_[0][1] 
-    Ts.append((j, Ts_[:5]))
+    Ts.append((j, Ts_[:min(max_reflections, len(Ts_))]))
 
-  olx.Freeze(True)
+  fm_header = "#%3s%4s%4s%8s%10s%10s\n"
+  if not output_to:
+    output_to = sys.stdout
+    fm_hkl = "%4i%4i%4i%8.2f%10.3f%10.3f\n"
+  else:
+    fm_hkl = "%i %i %i %.2f %.3f %.3f\n"
+  output_to.write(fm_header %("H", "K", "L", "V", "Fo_sq", "Fc_sq"))
   for j, Ts_ in Ts:
-    print("For " + labels[j])
-    for i in xrange(0,5):
+    output_to.write("#%s\n" %(labels[j]))
+    for i in xrange(0, len(Ts_)):
+      idx = self.observations.indices[Ts_[i][0]]
       val = Ts_[i][1]*100/maxT
-      print(str(self.observations.indices[Ts_[i][0]]) + \
-             ": %.2f. Obs: %.3f, Calc: %.3f" %(val, self.observations.data[Ts_[i][0]]/scale_factor,
-                                          result.observables()[Ts_[i][0]]))
-  olx.Freeze(False)
-
-  self.f_calc = self.observations.fo_sq.array(
-    data=result.f_calc(), sigmas=None)
-  self.fc_sq = self.observations.fo_sq.array(
-    data=result.observables(), sigmas=None)\
-      .set_observation_type_xray_intensity()
-  self.objective_data_only = self.objective()
-  if False and self.restraints_manager is not None:
-    if self.restraints_normalisation_factor is None:
-      self.restraints_normalisation_factor \
-          = 2 * self.objective_data_only/(ds_mat.shape[0]-ds_mat.shape[1])
-    linearised_eqns = self.restraints_manager.build_linearised_eqns(
-      self.xray_structure, self.reparametrisation.parameter_map())
-    jacobian = \
-      self.reparametrisation.jacobian_transpose_matching(
-        self.reparametrisation.mapping_to_grad_fc_all).transpose()
-    self.reduced_problem().add_equations(
-      linearised_eqns.deltas,
-      linearised_eqns.design_matrix * jacobian,
-      linearised_eqns.weights * self.restraints_normalisation_factor)
-    self.n_restraints = linearised_eqns.n_restraints()
-    self.chi_sq_data_and_restraints = self.chi_sq()
-  if False: #not objective_only:
-    self.origin_fixing_restraint.add_to(
-      self.step_equations(),
-      self.reparametrisation.jacobian_transpose_matching_grad_fc(),
-      self.reparametrisation.asu_scatterer_parameters)
-
-class flack_leverage(object):
-  def __init__(self, connectivity_table=None):
-    import numpy as np
-    import scipy.linalg as scla
-    from cctbx_olex_adapter import OlexCctbxAdapter, OlexCctbxMasks
-    from cctbx.xray import observations
-    from cctbx import sgtbx
-    from smtbx.refinement import constraints
-    from smtbx.refinement import least_squares
-    from scitbx.lstbx import normal_eqns_solving
-
-    adaptor = OlexCctbxAdapter()
-    if adaptor.exti is None:
-      adaptor.exti = xray.dummy_extinction_correction()
-    else:
-      adaptor,exti.grad = False
-
-    #!! read the FAB !!
-    f_mask = flex.complex_double()
-
-    obs_ = adaptor.observations
-    assert obs_.fo_sq.anomalous_flag()
-    xray_structure = adaptor.xray_structure()
-    flags = xray_structure.scatterer_flags()
-    for sc in xray_structure.scatterers():
-      f = xray.scatterer_flags()
-      f.set_use_u_aniso(sc.flags.use_u_aniso())
-      f.set_use_u_iso(sc.flags.use_u_iso())
-      f.set_use_fp_fdp(True)
-      sc.flags = f
-
-    twin_fractions = obs_.twin_fractions
-    twin_components = obs_.merohedral_components
-    for tw in twin_fractions: tw.grad = False
-    for tc in twin_components: tc.grad = False
-
-    it = xray.twin_component(sgtbx.rot_mx((-1,0,0,0,-1,0,0,0,-1)), 0.2, True)
-    twin_components += (it,)
-    obs = observations.customized_copy(obs_, twin_fractions, twin_components)
-    # reparameterisation needs all fractions
-    twin_fractions += twin_components
-    connectivity_table = adaptor.connectivity_table
-    reparametrisation = constraints.reparametrisation(
-      xray_structure, [], connectivity_table,
-      twin_fractions=twin_fractions,
-      extinction=adaptor.exti
-    )
-    normal_eqns = least_squares.crystallographic_ls(obs,
-      reparametrisation)
-
-    def args(scale_factor, weighting_scheme):
-      args = (normal_eqns,
-              normal_eqns.observations,
-              f_mask,
-              weighting_scheme,
-              scale_factor,
-              normal_eqns.one_h_linearisation,
-              normal_eqns.reparametrisation.jacobian_transpose_matching_grad_fc(),
-              adaptor.exti)
-      return args
-
-    scale_factor = float(olx.xf.rm.OSF())
-    scale_factor *= scale_factor
-###################
-    cycles = normal_eqns_solving.naive_iterations(
-      normal_eqns, n_max_iterations=10,
-      gradient_threshold=1e-7,
-      step_threshold=1e-4)
-
-    normal_eqns.reset()
-
-    result = ext.build_design_matrix(
-      *args(scale_factor=scale_factor, weighting_scheme=sigma_weighting()))
-    ds_mat = result.design_matrix()
-    ds_mat = ds_mat.as_numpy_array()
-    for r in xrange(0, ds_mat.shape[0]):
-      ds_mat[r,:] *= math.sqrt(result.weights()[r])
-    #self.weights = scla.block_diag([math.sqrt(x) for x in result.weights()])
-    Z_mat = ds_mat #self.weights*ds_mat
-    Zi_mat = scla.inv(Z_mat.T.dot(Z_mat))
-    Pp_mat = Z_mat.dot(Zi_mat).dot(Z_mat.T)
-    t_mat = Z_mat.dot(Zi_mat)
-    t_mat = t_mat**2
-    for i in xrange(0, t_mat.shape[0]):
-      t_mat[i,:] /= (1+Pp_mat[i][i])
-    Ts = []
-    maxT = 0
-    for j in xrange(0, t_mat.shape[1]):
-      Ts_ = [(xn, x) for xn, x in enumerate(t_mat[:,j])]
-      Ts_.sort(key=lambda x: x[1],reverse=True)
-      if Ts_[0][1] > maxT:
-        maxT = Ts_[0][1] 
-      Ts.append((j, Ts_[:100]))
-
-    for j, Ts_ in Ts:
-      print("For Flack")
-      for i in xrange(0,100):
-        val = Ts_[i][1]*100/maxT
-        print(str(normal_eqns.observations.indices[Ts_[i][0]]) + \
-               ": %.2f. Obs: %.3f, Calc: %.3f" %(val, normal_eqns.observations.data[Ts_[i][0]]/scale_factor,
-                                            result.observables()[Ts_[i][0]]))
-#############################
-    self.flack_x = it.value
-    self.sigma_x = math.sqrt(normal_eqns.covariance_matrix(
-      jacobian_transpose=reparametrisation.jacobian_transpose_matching(
-        reparametrisation.mapping_to_grad_fc_independent_scalars))[0])
-    self.show()
-
-
-  def show(self, out=None):
-    from libtbx.utils\
-      import format_float_with_standard_uncertainty as format_float_with_su
-    if out is None: out = sys.stdout
-    print >> out, "Flack x: %s" %format_float_with_su(self.flack_x, self.sigma_x)
-
+      output_to.write(fm_hkl %(idx[0], idx[1], idx[2],\
+        val, self.observations.data[Ts_[i][0]]/scale_factor, result.observables()[Ts_[i][0]]))
+  if False:
+    self.f_calc = self.observations.fo_sq.array(
+      data=result.f_calc(), sigmas=None)
+    self.fc_sq = self.observations.fo_sq.array(
+      data=result.observables(), sigmas=None)\
+        .set_observation_type_xray_intensity()
+    self.objective_data_only = self.objective()
+    if self.restraints_manager is not None:
+      if self.restraints_normalisation_factor is None:
+        self.restraints_normalisation_factor \
+            = 2 * self.objective_data_only/(ds_mat.shape[0]-ds_mat.shape[1])
+      linearised_eqns = self.restraints_manager.build_linearised_eqns(
+        self.xray_structure, self.reparametrisation.parameter_map())
+      jacobian = \
+        self.reparametrisation.jacobian_transpose_matching(
+          self.reparametrisation.mapping_to_grad_fc_all).transpose()
+      self.reduced_problem().add_equations(
+        linearised_eqns.deltas,
+        linearised_eqns.design_matrix * jacobian,
+        linearised_eqns.weights * self.restraints_normalisation_factor)
+      self.n_restraints = linearised_eqns.n_restraints()
+      self.chi_sq_data_and_restraints = self.chi_sq()
+    if False: #not objective_only:
+      self.origin_fixing_restraint.add_to(
+        self.step_equations(),
+        self.reparametrisation.jacobian_transpose_matching_grad_fc(),
+        self.reparametrisation.asu_scatterer_parameters)
 
 leverage_obj = Leverage()
 olex.registerFunction(leverage_obj.calculate, False, "leverage")
 olex.registerFunction(leverage_obj.calculate_for, False, "leverage")
-olex.registerFunction(flack_leverage, False, "leverage")
+olex.registerFunction(leverage_obj.for_flack, False, "leverage")
