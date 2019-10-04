189f8420d490f6d9f53f3993d32a34c9ac192f05
pcxod <oleg_dolomanov@hotmail.com>
2019-10-04 12:53:16 +0000

    - recognising numerical abs correction type - updating p4p reader to extract extra information - not sure it even worked before!
    git-svn-id: http://svn.olex2.org/olex2-gui/trunk@5946 bceb41a0-0b25-4981-bef4-7ef80ce4c728

--- util/pyUtil/PyToolLib/FileReaders/p4p_reader.py
+++ util/pyUtil/PyToolLib/FileReaders/p4p_reader.py
@@ -6,54 +6,36 @@ class p4p_reader:
 
   def read_p4p(self):
     """Reads the .p4p file with the given path.
-
     Returns a dictionary of cif items found in the p4p file."""
 
-    rFile = open(self.path, 'r')
-    p4p = []
-    for line in rFile:
-      p4p.append(line)
-    p4p_key = {"raw":{}, "cif":{}}
-    i = 0
-    for li in p4p:
+    cif = {}
+    for li in open(self.path, 'r').readlines():
       li = li.strip()
       if not li:
         continue
       if li[:2] == "  ":
         continue
       l = li.split()
-      field = l[0].strip()
-      value = li.split(field)[1].strip()
-      if field != "REF05":
-        p4p_key["raw"].setdefault(field, value)
-      else: break
+      key, val = l[0], l[1:]
+      if key == "REF05":
+        break
+      elif key == "SOURCE" and len(val) == 7:
+        try:
+          cif["_diffrn_source_voltage"] = float(val[5])
+          cif["_diffrn_source_current"] = float(val[6])
+        except:
+          pass
+      elif key == "CCOLOR": 
+        cif["_exptl_crystal_colour"] = val[0]
+      elif key == "MORPH": 
+        cif["_exptl_crystal_description"] = val[0]
+      if len(cif) == 4:
+        break
 
+    if "_diffrn_source_voltage" in cif and "_diffrn_source_current" in cif:
+      cif["_diffrn_source_power"] = (cif['_diffrn_source_voltage'] * cif['_diffrn_source_current'])/1000
 
-    if p4p_key["cif"].has_key('raw'):
-      ciflist=["_diffrn_source","_diffrn_source_voltage","_diffrn_source_current",]
-      have_cif_item = False
-      value = ""
-      for item in ciflist:
-        if item == "_diffrn_source_voltage":
-          try:
-            if p4p_key["raw"]["SOURCE"]:
-              value = float(p4p_key["raw"]["SOURCE"].split()[5])
-          except:
-            value = "n/a"
-        elif item == "_diffrn_source_current":
-          try:
-            if p4p_key["raw"]["SOURCE"]:
-              value = float(p4p_key["raw"]["SOURCE"].split()[6])
-          except:
-            value = "n/a"
-        if value:
-          p4p_key["cif"].setdefault(item, value)
-
-    if p4p_key["cif"].has_key('_diffrn_source_voltage') and p4p_key["cif"].has_key('_diffrn_source_current'):
-      if p4p_key["cif"]['_diffrn_source_voltage'] != "n/a" or p4p_key["cif"]['_diffrn_source_current'] != "n/a":
-        p4p_key["cif"].setdefault('_diffrn_source_power', (p4p_key["cif"]['_diffrn_source_voltage'] * p4p_key["cif"]['_diffrn_source_current'])/1000)
-
-    return p4p_key["cif"]
+    return cif
 
 if __name__ == '__main__':
   a = p4p_reader('C:/datasets/08srv071/first.p4p')
