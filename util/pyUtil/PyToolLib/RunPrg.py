a933db891757425394f80d68beea9daeed093423
horst <horst@olexsys.org>
2020-03-06 16:11:21 +0000

    move resetting NSFF DPR to the right place (hopefully!)
    git-svn-id: http://svn.olex2.org/olex2-gui/trunk@6057 bceb41a0-0b25-4981-bef4-7ef80ce4c728

--- util/pyUtil/PyToolLib/RunPrg.py
+++ util/pyUtil/PyToolLib/RunPrg.py
@@ -348,8 +348,6 @@ class RunPrg(ArgumentParser):
 
   def startRun(self):
     OV.CreateBitmap('%s' %self.bitmap)
-    OV.SetParam('snum.refinement.data_parameter_ratio', 0)
-    OV.SetParam('snum.NoSpherA2.use_aspherical', False)
     LM.startRun(self)
 
   def endRun(self):
@@ -406,6 +404,8 @@ class RunSolutionPrg(RunPrg):
             self.terminate = True
             return
 
+    OV.SetParam('snum.refinement.data_parameter_ratio', 0)
+    OV.SetParam('snum.NoSpherA2.use_aspherical', False)
     self.startRun()
     OV.SetParam('snum.refinement.auto.invert',True)
     if OV.IsFileType('cif'):
