3d25224cb6e0a7c0e53ca34757188bc63e57561d
horst <horst@olexsys.org>
2020-03-15 13:40:46 +0000

    run the help/md thing on kickstarter and not straight off for plugins

--- util/pyUtil/PluginLib/PluginTools.py
+++ util/pyUtil/PluginLib/PluginTools.py
@@ -31,6 +31,7 @@ class PluginTools(VFSDependent):
     p = os.path.join(self.p_path, self.p_htm + ".htm")
     if OV.HasGUI() and make_gui:
       try:
+        gui.help.gh.git_help(quick=False, specific=self.p_path)
         t = gui.file_open(p, 'r')
         OlexVFS.write_to_olex(p, t)
         olx.html.Update()
@@ -105,7 +106,7 @@ class PluginTools(VFSDependent):
                           before=before, filetype='', image=image, onclick=self.p_onclick)
       except:
         pass
-    gui.help.gh.git_help(quick=False, specific=self.p_path)
+    #gui.help.gh.git_help(quick=False, specific=self.p_path)
 
   def edit_customisation_folder(self,custom_name=None):
     self.get_customisation_path(custom_name=None)
