a0169d9415414433d03eec4e81c09d57567de23f
horst <horst@olexsys.org>
2023-10-13 11:01:32 +0000

    Hopefully fix the multiple CIF display -- make sure it is shown only in the correct structures, and sorted by ccdc number (if present)

--- util/pyUtil/gui/home/__init__.py
+++ util/pyUtil/gui/home/__init__.py
@@ -4,14 +4,14 @@ import OlexVFS
 import os
 from olexFunctions import OV
 
-global CURR_CIF_FILE_NAME
-CURR_CIF_FILE_NAME = None
+#global CURR_CIF_FILE_NAME
+#CURR_CIF_FILE_NAME = None
 
-global CURR_CIF_FILE_LIST
-CURR_CIF_FILE_LIST = []
+#global CURR_CIF_FILE_LIST
+#CURR_CIF_FILE_LIST = []
 
-global CURR_CIF_FILE_FOLDER
-CURR_CIF_FILE_FOLDER = None
+#global CURR_CIF_FILE_FOLDER
+#CURR_CIF_FILE_FOLDER = None
 
 
 def BGColorForValue(value):
@@ -22,46 +22,49 @@ def BGColorForValue(value):
 
 class MultipleDataset:
   def __init__(self):
-    self.CURR_CIF_FILE_NAME = CURR_CIF_FILE_NAME
-    self.CURR_CIF_FILE_FOLDER = CURR_CIF_FILE_FOLDER
-    self.CURR_CIF_FILE_LIST = CURR_CIF_FILE_LIST
-  
+    #self.CURR_CIF_FILE_NAME = CURR_CIF_FILE_NAME
+    #self.CURR_CIF_FILE_FOLDER = CURR_CIF_FILE_FOLDER
+    #self.CURR_CIF_FILE_LIST = CURR_CIF_FILE_LIST
+    self.CURR_CIF_FILE_NAME = None
+    self.CURR_CIF_FILE_FOLDER = None
+    self.CURR_CIF_FILE_LIST = []
+
   def check(self):
-    if CURR_CIF_FILE_FOLDER:
-      if CURR_CIF_FILE_FOLDER != OV.FilePath():
+    if self.CURR_CIF_FILE_FOLDER:
+      if self.CURR_CIF_FILE_FOLDER != OV.FilePath():
         return False
-    if CURR_CIF_FILE_NAME:
-      self.CURR_CIF_FILE_NAME = CURR_CIF_FILE_NAME
-      return True
+    #if self.CURR_CIF_FILE_NAME:
+      ##self.CURR_CIF_FILE_NAME = CURR_CIF_FILE_NAME
+      #return True
     if olx.IsFileType('cif') == 'false':
       return False
     if int(olx.xf.DataCount()) <= 1:
       return False
     return True
 
-  def generateHtml(self,make_always=False):
-    global CURR_CIF_FILE_NAME
-    global CURR_CIF_FILE_LIST
-    global CURR_CIF_FILE_FOLDER
+  def generateHtml(self, make_always=False, sort_key='_database_code_depnum_ccdc_archive'):
+    #global CURR_CIF_FILE_NAME
+    #global CURR_CIF_FILE_LIST
+    #global CURR_CIF_FILE_FOLDER
     current = None
     html = '<table border="0" VALIGN="center" width="100%" cellpadding="1" cellspacing="0" bgcolor="$GetVar(HtmlTableRowBgColour)"><tr>'
-    
+
     if olx.IsFileType('cif') == 'true':
-      if CURR_CIF_FILE_NAME != OV.FileFull():
-        CURR_CIF_FILE_NAME = None
-        CURR_CIF_FILE_LIST = []
+      if self.CURR_CIF_FILE_NAME != OV.FileFull():
+        self.CURR_CIF_FILE_NAME = None
+        self.CURR_CIF_FILE_LIST = []
 
-    if not CURR_CIF_FILE_FOLDER:
-      if olx.IsFileType('cif') == 'true':
-        CURR_CIF_FILE_FOLDER = OV.FilePath()
+    if not self.CURR_CIF_FILE_FOLDER or self.CURR_CIF_FILE_FOLDER != OV.FilePath():
+      self.CURR_CIF_FILE_FOLDER = OV.FilePath()
 
-    if not CURR_CIF_FILE_NAME:
+    if not self.CURR_CIF_FILE_NAME or self.CURR_CIF_FILE_FOLDER != OV.FileName():
+      self.CURR_CIF_FILE_NAME = OV.FileFull()
       if olx.IsFileType('cif') == 'true':
-        CURR_CIF_FILE_NAME = OV.FileFull()
         current = int(olx.xf.CurrentData())
 
-    if not CURR_CIF_FILE_LIST:
+    if not self.CURR_CIF_FILE_LIST:
       cnt = int(olx.xf.DataCount())
+      sort = 0
       for i in range(0, cnt):
         if olx.IsFileType('cif') == 'true':
           if olx.xf.DataName(i) == "global" or not olx.xf.DataName(i):
@@ -70,53 +73,55 @@ class MultipleDataset:
         display = ""
         if olx.IsFileType('cif') == 'true':
           name = olx.xf.DataName(i)
+          sort = olx.Cif('%s#%i' % (sort_key, i))
         else:
           name = OV.FileName()
         if len(name) > 15:
-          display = "%s..%s" %(name[:6], name[-6:])
+          display = "%s..%s" % (name[:6], name[-6:])
         else:
           display = name
         current = 0
-        CURR_CIF_FILE_LIST.append((i, name, display))
+        self.CURR_CIF_FILE_LIST.append((i, name, display, sort))
     else:
-      cnt = len(CURR_CIF_FILE_LIST)
+      cnt = len(self.CURR_CIF_FILE_LIST)
       for i in range(0, cnt):
-        if OV.ModelSrc() in CURR_CIF_FILE_LIST[i]:
+        if OV.ModelSrc() in self.CURR_CIF_FILE_LIST[i]:
           current = i
           break
 
-
-    if (cnt%3) == 0:
-      td_width='33'
-    elif (cnt%4) == 0:
-      td_width='25'
-    elif (cnt%2) == 0:
-      td_width='50'
+    if (cnt % 3) == 0:
+      td_width = '33'
+    elif (cnt % 4) == 0:
+      td_width = '25'
+    elif (cnt % 2) == 0:
+      td_width = '50'
     else:
-      td_width='25'
+      td_width = '25'
+
+    self.CURR_CIF_FILE_LIST = sorted(self.CURR_CIF_FILE_LIST, key=lambda x: x[3])
 
     for i in range(0, cnt):
-      index = CURR_CIF_FILE_LIST[i][0]
-      name = CURR_CIF_FILE_LIST[i][1]
-      display = CURR_CIF_FILE_LIST[i][2]
-      if i > 0 and (i%4) == 0:
+      index = self.CURR_CIF_FILE_LIST[i][0]
+      name = self.CURR_CIF_FILE_LIST[i][1]
+      display = self.CURR_CIF_FILE_LIST[i][2]
+      if i > 0 and (i % 4) == 0:
         html += "</tr><tr width=100%>"
       if i == current:
-        bgcolour=OV.GetVar('HtmlBgColour')
+        bgcolour = OV.GetVar('HtmlBgColour')
         if OV.FileExt() == "cif":
-          reapfile = "%s%s" %(olx.xf.DataName(olx.xf.CurrentData()),".res")
+          reapfile = "%s%s" % (olx.xf.DataName(olx.xf.CurrentData()), ".res")
           if not os.path.exists(reapfile):
-            action = "export>>reap '%s'" %reapfile
+            action = "export>>reap '%s'" % reapfile
           else:
-            action = "reap '%s'" %reapfile
+            action = "reap '%s'" % reapfile
           highlight = olx.GetVar('HtmlHighlightColour')
           display = "** LOAD **"
         else:
-          action = 'reap %s#'%(CURR_CIF_FILE_NAME) + str(index)
+          action = 'reap %s#' % (self.CURR_CIF_FILE_NAME) + str(index)
           highlight = OV.GetParam('gui.green')
-          display = "CIF %s" %display
+          display = "CIF %s" % display
       else:
-        action = 'reap \'%s#'%(CURR_CIF_FILE_NAME) + str(index) + "'"
+        action = 'reap \'%s#' % (self.CURR_CIF_FILE_NAME) + str(index) + "'"
         highlight = olx.GetVar('linkButton.bgcolor')
       name = name.replace("(", "_").replace(")", "_")
       display = display.replace("(", "_").replace(")", "_")
@@ -128,13 +133,14 @@ class MultipleDataset:
       "onclick=%s",
       "bgcolor=%s",
       )
-    $-''' %(display, name, action, highlight)
+    $-''' % (display, name, action, highlight)
 
     html += "</tr></table>"
     #name = "%s_%s" %(os.path.split(CURR_CIF_FILE_NAME)[1].replace(' ', '_'), 'multicif.htm')
-    name = "%s_%s" %(CURR_CIF_FILE_NAME, 'multicif.htm')
+    name = "%s_%s" % (self.CURR_CIF_FILE_NAME, 'multicif.htm')
     OlexVFS.write_to_olex(name, html)
-    return "<!-- #include multicif %s;1; -->" %name
+    return "<!-- #include multicif %s;1; -->" % name
+
 
 mds = MultipleDataset()
 olex.registerFunction(mds.check, False, "gui.home.multiple_dataset")
