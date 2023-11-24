643afacb5402fdcd0d283d32a631ef56339d13b8
oleg <oleg@olexsys.org>
2023-11-24 17:50:39 +0000

    - final touches to MuC - making sure the CIF index persists while loading RES

--- util/pyUtil/gui/home/__init__.py
+++ util/pyUtil/gui/home/__init__.py
@@ -11,12 +11,17 @@ def BGColorForValue(value):
 
 
 class MultipleDataset:
+  datasets = None
+  inc_txt = "<!-- #include multicif multicif.htm;1; -->"
+  cif_name = None
+  cif_mk_time = None
+
   def __init__(self):
     pass
 
   def check(self):
     if olx.IsFileType('cif') != 'true':
-      return False
+      return self.find_file_index() != None
     cnt = int(olx.xf.DataCount())
     useful = 0
     for i in range(cnt):
@@ -45,19 +50,42 @@ class MultipleDataset:
       rv.append((i, name, display, sort, True))
     return rv
 
-  def generateHtml(self, make_always=False, sort_key='_database_code_depnum_ccdc_archive'):
-    if olx.IsFileType('cif') != 'true':
-      return ""
+  def find_file_index(self):
+    if not self.datasets or not self.cif_name:
+      return None
+    cif_mk_time = os.path.getmtime(self.cif_name)
+    if cif_mk_time != self.cif_mk_time:
+      return None
+    op = os.path.split(self.cif_name)[0]
+    cp = os.path.split(olx.FileFull())[0]
+    if op == cp:
+      file_name = olx.FileName()
+      for index, name, display, sk, do_show in self.datasets:
+        if name == file_name:
+          return  index
+    return None
 
-    current = int(olx.xf.CurrentData())
-    file_name = olx.FileFull()
+  def generateHtml(self, make_always=False, sort_key='_database_code_depnum_ccdc_archive'):
+    current = None
+    if olx.IsFileType('cif') != 'true':
+      current = self.find_file_index()
+      if current is None:
+        self.cif_name, self.cif_mk_time, self.datasets = None, None, None
+        return ""
+
+    if current is None:
+      current = int(olx.xf.CurrentData())
+      self.datasets = self.list_datasets(sort_key=sort_key)
+      self.datasets.sort(key=lambda x: x[3])
+      self.cif_name = olx.FileFull()
+      self.cif_mk_time = os.path.getmtime(self.cif_name)
+
     html = '<table border="0" VALIGN="center" width="100%" cellpadding="1" cellspacing="0" bgcolor="$GetVar(HtmlTableRowBgColour)"><tr>'
 
-    datasets = self.list_datasets(sort_key=sort_key)
-    datasets.sort(key=lambda x: x[3])
     shown_cnt = 0
+    is_CIF = OV.FileExt() == "cif"
 
-    for index, name, display, sk, do_show in datasets:
+    for index, name, display, sk, do_show in self.datasets:
       if not do_show:
         continue
       shown_cnt += 1
@@ -65,25 +93,25 @@ class MultipleDataset:
       if shown_cnt > 1 and ((shown_cnt-1) % 4) == 0:
         html += "</tr><tr width=100%>"
       if index == current:
-        bgcolour = OV.GetVar('HtmlBgColour')
-        if OV.FileExt() == "cif":
-          reapfile = "%s%s" % (olx.xf.DataName(current), ".res")
+        if is_CIF:
+          reapfile = os.path.join(olx.FilePath(), olx.xf.DataName(current)) + ".res"
           if not os.path.exists(reapfile):
             action = "export>>reap '%s'" % reapfile
             display = "*EXP/LOAD*"
             highlight = olx.GetVar('HtmlHighlightColour')
           else:
+            action = "reap '%s'" % reapfile
             highlight = olx.GetVar('gui.blue')
             fg = '#ffffff'
             display = "*LOAD RES*"
         else:
+          action = 'reap \'%s#%s\'' % (self.cif_name, index)
           highlight = OV.GetParam('gui.green')
           fg = '#ffffff'
           display = "CIF %s" % display
       else:
-        action = 'reap \'%s#%s\'' % (file_name, index)
+        action = 'reap \'%s#%s\'' % (self.cif_name, index)
         highlight = olx.GetVar('linkButton.bgcolor')
-      action = 'reap \'%s#%s\'' % (file_name, index)
       name = name.replace("(", "_").replace(")", "_")
       display = display.replace("(", "_").replace(")", "_").replace("_0m_a", ".").replace("_auto", ".")
       html += '''
@@ -98,9 +126,8 @@ class MultipleDataset:
     $-''' % (display, name, action, highlight, fg)
 
     html += "</tr></table>"
-    name = "%s" % ('multicif.htm')
-    OlexVFS.write_to_olex(name, html)
-    return "<!-- #include multicif %s;1; -->" % name
+    OlexVFS.write_to_olex("multicif.htm", html)
+    return self.inc_txt
 
 
 mds = MultipleDataset()
