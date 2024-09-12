f9b177b0f60f775df1681913412c52aaa2f80a78
pcxod <oleg_dolomanov@hotmail.com>
2024-09-12 12:38:13 +0000

    - more on client mode and server sharing... work in progress
    git-svn-id: http://svn.olex2.org/olex2-gui/trunk@7018 bceb41a0-0b25-4981-bef4-7ef80ce4c728

--- util/pyUtil/PyToolLib/method_imp/client.py
+++ util/pyUtil/PyToolLib/method_imp/client.py
@@ -16,47 +16,68 @@ class Method_client_refinement(Method_refinement):
 
     super(Method_client_refinement, self).__init__(phil_object)
 
-  def send_cmd(self, host, port, cmd):
+  def send_cmd(self, host, port, cmd, handle=True):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
       try:
         s.connect((host, port))
         s.sendall(cmd)
         return s.recv(1024)
       except ConnectionRefusedError as ex:
-        return None
+        if handle:
+          return None
+        else:
+          raise ex
       except OSError as ex: # socket closed?
-        return None
+        if handle:
+          return None
+        else:
+          raise ex
 
   def pre_refinement(self, RunPrgObject):
+    import uuid
+    self.job_id = uuid.uuid4().hex
     Method_refinement.pre_refinement(self, RunPrgObject)
     host = OV.GetParam("user.Server.host")
     port = OV.GetParam("user.Server.port")
     debug = OV.GetParam("user.Server.debug")
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-      try:
-        s.connect((host, port))
-        s.sendall(b"status\n")
-        data = s.recv(1024).decode("utf-8").rstrip('\n')
+    share = OV.GetParam("user.Server.share_localhost")
+    try:
+        reserve_ = ("reserve:%s\n" %self.job_id).encode()
+        data = self.send_cmd(host, port, reserve_, handle=False).decode("utf-8").rstrip('\n')
         print(f"Received {data}")
+        bmp_created = False
+        bmp_name = "Server is busy"
         while data == "busy":
-          time.sleep(0.5)
-          s.sendall(b"status\n")
-          data = s.recv(1024).decode("utf-8").rstrip('\n')
-          print(f"Received {data}")
-
+          if not share:
+            port += 1
+            data = self.send_cmd(host, port, reserve_, handle=False).decode("utf-8").rstrip('\n')
+            continue
+          if not bmp_created:
+            OV.CreateBitmap(bmp_name)
+            bmp_created = True
+          time.sleep(1)
+          olx.Refresh()
+          data = self.send_cmd(host, port, reserve_, handle=True).decode("utf-8").rstrip('\n')
+        if bmp_created:
+          OV.DeleteBitmap(bmp_name)
         if data == "ready":
           print("Using Olex2 server on %s" %port)
-      except ConnectionRefusedError as ex:
-        print("Launching Olex2 server...")
-        cd = os.curdir
-        os.chdir(olx.BaseDir())
-        my_env = os.environ.copy()
-        if "OLEX2_ATTACHED_WITH_PYDEBUGGER" in my_env and debug != "Wing":
-          del my_env["OLEX2_ATTACHED_WITH_PYDEBUGGER"]
-        subprocess.Popen(
-          [os.path.join(olx.BaseDir(), "olex2c.dll"), "server", str(port)],
-          env=my_env)
-        os.chdir(cd)
+        OV.SetVar("server.port", str(port))
+    except ConnectionRefusedError as ex:
+      if host != "localhost":
+        raise Exception("No running server found!")
+      print("Launching Olex2 server...")
+      cd = os.curdir
+      os.chdir(olx.BaseDir())
+      my_env = os.environ.copy()
+      if "OLEX2_ATTACHED_WITH_PYDEBUGGER" in my_env and debug != "Wing":
+        del my_env["OLEX2_ATTACHED_WITH_PYDEBUGGER"]
+      subprocess.Popen(
+        [os.path.join(olx.BaseDir(), "olex2c.dll"), "server", str(port), self.job_id],
+        env=my_env)
+      os.chdir(cd)
+      OV.SetVar("server.port", str(port))
+      OV.SetVar("launched_server.port", str(port))
 
   def read_log_markup(self, log, marker):
     rv = []
@@ -83,7 +104,7 @@ class Method_client_refinement(Method_refinement):
           if marker == "info":
             continue
           if marker in ("error", "warning", "exception"):
-            olx.Echo('\n'.join(txt), m="error")
+            olx.Echo('\n'.join(txt), m=marker)
           else:
             print('\n'.join(txt))
         else:
@@ -91,13 +112,17 @@ class Method_client_refinement(Method_refinement):
 
   def do_run(self, RunPrgObject):
     host = OV.GetParam("user.Server.host")
-    port = OV.GetParam("user.Server.port")
+    port = int(OV.GetVar("server.port"))
+    #port = OV.GetParam("user.Server.port")
     log_fn = os.path.join(OV.StrDir(), "olex2.refine_srv.log")
+    if os.path.exists(log_fn):
+      os.remove(log_fn)
     inp_fn = os.path.join(RunPrgObject.filePath, RunPrgObject.original_filename)
     debug = OV.GetParam("user.Server.debug")
-    cmds = ["run:",
+    cmds = ["run:%s" %self.job_id,
             "xlog:%s" %log_fn,
             "spy.DebugInVSC" if debug=="VSC" else "",
+            "SetVar server.job_id " + self.job_id,
             "SetVar olex2.remote_mode true",
             "spy.SetParam user.refinement.client_mode False",
             "SetOlex2RefinementListener(True)",
