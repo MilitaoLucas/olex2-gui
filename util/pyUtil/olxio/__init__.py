ca713b607513505252174784940fc36a6fbe1fe2
pcxod <oleg_dolomanov@hotmail.com>
2024-11-10 15:33:41 +0000

    - trying to put timing under control and unload initpy...

--- util/pyUtil/olxio/__init__.py
+++ util/pyUtil/olxio/__init__.py
@@ -1,4 +1,5 @@
-import os
+import sys, os
+import olex
 
 class FileId(object):
   """ a simple object to compare files size and timestamp
@@ -20,3 +21,64 @@ class FileId(object):
 
   def ok(self) -> bool:
     return self.mk_time != 0 and len(self.name) > 0
+
+class StreamRedirection:
+  def __init__(self, stream, basedir, datadir, is_redirecting=True):
+    self.redirected = stream
+    self.basedir = basedir
+    self.datadir = datadir
+    self.is_redirecting = is_redirecting
+    self.isErrorStream = (stream==sys.stderr)
+    self.graph=False
+
+    if self.isErrorStream:
+      self.errFile = open(os.path.join(self.datadir, "PythonError.log"), 'w', encoding="utf8")
+      self.version = olex.f("GetCompilationInfo()")
+      try:
+        self.GUIversion = open(os.path.join(self.basedir, "version.txt"), 'r').readline()
+      except:
+        self.GUIversion = "unknown"
+      self.errFile.write("================= PYTHON ERROR ================= Olex2 Version %s -- %s\n\n" %(self.version, self.GUIversion))
+
+  def write(self, Str):
+    if self.is_redirecting:
+      if self.isErrorStream:
+        self.errFile.write(Str)
+        self.errFile.flush()
+      olex.post( '\'' + Str + '\'')
+      if self.graph!=False:
+        self.graph(Str)
+    else:
+      self.redirected.write(Str)
+
+  def flush(self):
+    pass
+
+  def formatExceptionInfo(self, maxTBlevel=5):
+    import traceback
+    import inspect
+    import tokenize
+    from olexFunctions import OV
+    traceback.print_exc()
+    tb = sys.exc_info()[2]
+    if OV.HasGUI():
+      olex.m("Cursor")
+    if tb is not None:
+      while tb.tb_next is not None: tb = tb.tb_next
+      frame = tb.tb_frame
+      def reader():
+        try:
+          yield inspect.getsource(frame)
+        except:
+          print(">>>>> ERROR (formatExceptionInfo)")
+      args = {}
+      try:
+        for ttype, token, start, end, line in inspect.tokenize.generate_tokens(reader().__next__):
+          if ttype == tokenize.NAME and token in frame.f_locals:
+            args[token] = frame.f_locals[token]
+        if args:
+          sys.stderr.write('Key variable values:\n')
+          for var,val in list(args.items()):
+            sys.stderr.write('\t%s = %s\n' % (var, repr(val)))
+      except inspect.tokenize.TokenError:
+        pass
