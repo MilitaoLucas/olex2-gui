2e28422d69fd0d9855dec70aa487154161a5ce59
FlorianKleemiss <florian.kleemiss@gmail.com>
2025-04-01 21:47:18 +0000

    update on html.ItemState
    git-svn-id: http://svn.olex2.org/olex2-gui/trunk@7202 bceb41a0-0b25-4981-bef4-7ef80ce4c728

--- util/pyUtil/CctbxLib/aaff.py
+++ util/pyUtil/CctbxLib/aaff.py
@@ -366,7 +366,7 @@ Please select one of the generators from the drop-down menu.""", "O", False)
   OV.SetParam('snum.NoSpherA2.Calculate', False)
   ext_name = "h3-NoSpherA2-extras"
   if OV.IsHtmlItem(ext_name):
-    olex.m("html.ItemState %s 2" %ext_name)
+    olex.m(f"html.ItemState {ext_name} 2")
   if converged == False:
     HAR_log.write(" !!! WARNING: UNCONVERGED MODEL! PLEASE INCREASE MAX_CYCLE OR CHECK FOR MISTAKES !!!\n")
     self.refinement_has_failed.append("Warning: Unconverged Model!")
