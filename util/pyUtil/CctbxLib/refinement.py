7274b7a4dd792e3254045acc1f518407183a09e9
horst <horst@olexsys.org>
2025-10-07 13:49:04 +0000

    try not to take a float of things like the word 'solution'
    git-svn-id: http://svn.olex2.org/olex2-gui/trunk@7339 bceb41a0-0b25-4981-bef4-7ef80ce4c728

--- util/pyUtil/CctbxLib/refinement.py
+++ util/pyUtil/CctbxLib/refinement.py
@@ -1725,11 +1725,16 @@ class FullMatrixRefine(OlexCctbxAdapter):
 
     if log is None: log = sys.stdout
 
-    last = float(OV.GetParam('snum.refinement.last_R1'))
+    last = OV.GetParam('snum.refinement.last_R1')
+    try:
+      last =  f"{last:.4f}"
+    except:
+      last = last
+
     pad = 2 - len(str(self.cycles.n_iterations))
     print("\n  ++++++++++++++++++++++++++++++++++++++++++++++++%s+++ After %i CYCLE%s +++" %(pad*"+", self.cycles.n_iterations, plural), file=log)
     #print >> log, " +"
-    print(f"  +  R1:       {self.r1[0]:.4f} for {self.r1[1]} reflections I >= 2u(I). Last R1: {last:.4f}", file=log)
+    print(f"  +  R1:       {self.r1[0]:.4f} for {self.r1[1]} reflections I >= 2u(I). Last R1: {last}", file=log)
     print("  +  R1 (all): %.4f for %i reflections" %self.r1_all_data, file=log)
 
     print("  +  wR2:      %.4f, GooF:  %.4f" % (
