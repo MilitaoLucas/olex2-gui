303c3e850ff4af8ddc6a3bbdafd9f14916418cb5
horst <horst@olexsys.org>
2025-11-13 17:49:53 +0000

    show python version only if OV.IsDebugging()
    git-svn-id: http://svn.olex2.org/olex2-gui/trunk@7393 bceb41a0-0b25-4981-bef4-7ef80ce4c728

--- util/pyUtil/PyToolLib/PilTools.py
+++ util/pyUtil/PyToolLib/PilTools.py
@@ -2694,7 +2694,8 @@ spy.doBanner(GetVar(snum_refinement_banner_slide))
       image.paste(sg, ((width * self.scale) - s[0], top), mask=a)
       top -= 1 * self.scale
       image = self.print_text(image, item, top, left, font_name, font_size, valign, halign, int(width-s[0]), font_colour, item_type)
-      image = self.print_text(image, f"{sys.version_info.major}.{sys.version_info.minor}", top, 300, font_name, font_size, valign, halign, int(width-s[0]), IT.adjust_colour(grad_colour, luminosity = 1.3), item_type="")
+      if OV.IsDebugging():
+        image = self.print_text(image, f"{sys.version_info.major}.{sys.version_info.minor}", top, 300, font_name, font_size, valign, halign, int(width-s[0]), IT.adjust_colour(grad_colour, luminosity = 1.3), item_type="")
 
     if self.advertise_new:
       draw = ImageDraw.Draw(image)
